3a9ffc98fe1bdc43ef89fe0489545dd9289db3a2
Nathan Wang <nathan.r.wang@gmail.com>
2020-06-19 14:32:51 -0700

    update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,10 @@ export default function IndexPage(props: PageProps) {
           <main className="mt-8 mx-auto max-w-6xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
             <div className="lg:grid lg:grid-cols-12 lg:gap-8">
               <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:flex-col lg:justify-center">
+                <div
+                  className="text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
+                  Pre-Release
+                </div>
                 <h2
                   className="mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
                   USACO <span className="text-blue-600">Guide</span>
@@ -87,7 +91,7 @@ export default function IndexPage(props: PageProps) {
       </div>
       {/* End Hero */}
 
-      <div className="py-12 bg-white">
+      <div className="py-12 bg-white" id="learn-more">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="lg:text-center">
             <p className="text-base leading-6 text-blue-600 font-semibold tracking-wide uppercase">About This Guide</p>
@@ -98,16 +102,37 @@ export default function IndexPage(props: PageProps) {
             <p className="mt-4 max-w-4xl text-xl leading-7 text-gray-500 lg:mx-auto">
               This is more than "just another resource." This is the first-ever comprehensive, organized roadmap carefully designed and crafted for USACO contestants – available to everyone, for free.
             </p>
+
+            <div className="max-w-3xl mx-auto bg-yellow-50 rounded-md border-yellow-400 border p-4 mt-8">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <svg className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd"/>
+                  </svg>
+                </div>
+                <div className="ml-3 flex-1 md:flex md:justify-between">
+                  <p className="text-sm leading-5 text-yellow-700">
+                    This guide is not a syllabus. Contest topics do not necessarily follow this guide.
+                  </p>
+                  <p className="mt-3 text-sm leading-5 md:mt-0 md:ml-6">
+                    <a href="#"
+                       className="whitespace-no-wrap font-medium text-yellow-700 hover:text-yellow-600 transition ease-in-out duration-150">
+                      Learn More &rarr;
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="mt-12">
+          <div className="mt-16">
             <ul className="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
               <li>
                 <div className="flex">
                   <div className="flex-shrink-0">
                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                       <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                       </svg>
                     </div>
@@ -125,7 +150,7 @@ export default function IndexPage(props: PageProps) {
                   <div className="flex-shrink-0">
                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                       <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
                       </svg>
                     </div>
@@ -143,7 +168,7 @@ export default function IndexPage(props: PageProps) {
                   <div className="flex-shrink-0">
                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                       <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="M13 10V3L4 14h7v7l9-11h-7z" />
                       </svg>
                     </div>
@@ -160,17 +185,15 @@ export default function IndexPage(props: PageProps) {
                 <div className="flex">
                   <div className="flex-shrink-0">
                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                      <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                      <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"/>
                       </svg>
                     </div>
                   </div>
                   <div className="ml-4">
-                    <h4 className="text-lg leading-6 font-medium text-gray-900">Join the Community
-                    </h4>
+                    <h4 className="text-lg leading-6 font-medium text-gray-900">Officially Recognized</h4>
                     <p className="mt-2 text-base leading-6 text-gray-500">
-                      Stuck on a problem? Have a burning question to ask? Join the <a href="#" className="text-blue-500 underline">USACO Forum</a> to get help from other USACO members.
+                      This guide is developed in collaboration with USACO Staff and USACO Director Dr. Brian Dean.
                     </p>
                   </div>
                 </div>
@@ -180,7 +203,7 @@ export default function IndexPage(props: PageProps) {
         </div>
       </div>
 
-      <div className="bg-blue-900" id="learn-more">
+      <div className="bg-blue-900">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 py-20 text-center">
           <p className="text-xl md:text-2xl font-bold text-blue-200">
             Unsure how to get started?<br/>
@@ -200,6 +223,64 @@ export default function IndexPage(props: PageProps) {
           </div>
         </div>
       </div>
+
+      {/* Begin FAQ */}
+      <div className="bg-white">
+        <div className="max-w-screen-xl mx-auto pt-12 pb-16 sm:pt-16 sm:pb-20 px-4 sm:px-6 lg:pt-20 lg:pb-28 lg:px-8">
+          <h2 className="text-3xl leading-9 font-extrabold text-gray-900">
+            Frequently asked questions
+          </h2>
+          <div className="mt-6 border-t-2 border-gray-100 pt-10">
+            <dl className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <div>
+                  <dt className="text-lg leading-6 font-medium text-gray-900">
+                    Is this a Syllabus?
+                  </dt>
+                  <dd className="mt-2">
+                    <p className="text-base leading-6 text-gray-500">
+                      <b>No. This guide is NOT a syllabus.</b> USACO does not have an official syllabus. This guide does not list all topics that may appear in each division and does not guarantee that certain topics belong to certain divisions.
+                    </p>
+                  </dd>
+                </div>
+                <div className="mt-12">
+                  <dt className="text-lg leading-6 font-medium text-gray-900">
+                    How can I contribute/report a problem?
+                  </dt>
+                  <dd className="mt-2">
+                    <p className="text-base leading-6 text-gray-500">
+                      If you would like to contribute towards this guide or report an issue, please contact Nathan Wang at <a className="text-blue-600 underline" href="mailto:nathan.r.wang@gmail.com">nathan.r.wang@gmail.com</a>.
+                    </p>
+                  </dd>
+                </div>
+              </div>
+              <div className="mt-12 md:mt-0">
+                <div>
+                  <dt className="text-lg leading-6 font-medium text-gray-900">
+                    Is this guide for Platinum contestants?
+                  </dt>
+                  <dd className="mt-2">
+                    <p className="text-base leading-6 text-gray-500">
+                      While we offer some resources that platinum contestants may find useful, the platinum division has too many topics for us to effectively cover. For platinum contestants, we recommend you treat this as more of a <i>resource</i> than a <i>guide</i>.
+                    </p>
+                  </dd>
+                </div>
+                <div className="mt-12">
+                  <dt className="text-lg leading-6 font-medium text-gray-900">
+                    How can I get help?
+                  </dt>
+                  <dd className="mt-2">
+                    <p className="text-base leading-6 text-gray-500">
+                      uh.....
+                    </p>
+                  </dd>
+                </div>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
+      {/*End FAQ*/}
     </Layout>
   )
 }
