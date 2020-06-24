c7071888c32eefeec664c651f7df9a52cfd2ee9a
Nathan Wang <nathan.r.wang@gmail.com>
2020-06-24 14:28:57 -0700

    migrate some problems to new format

--- content/models.ts
+++ content/models.ts
@@ -5,7 +5,10 @@ const sources = {
   "SPOJ": "https://www.spoj.com/problems/",
   "YS": "https://judge.yosupo.jp/problem/",
   "CF": "https://codeforces.com/",
-  "USACO": "http://www.usaco.org/index.php?page=viewproblem2&cpid=",
+  "Bronze": "http://www.usaco.org/index.php?page=viewproblem2&cpid=",
+  "Silver": "http://www.usaco.org/index.php?page=viewproblem2&cpid=",
+  "Gold": "http://www.usaco.org/index.php?page=viewproblem2&cpid=",
+  "Plat": "http://www.usaco.org/index.php?page=viewproblem2&cpid=",
   "Kattis": "https://open.kattis.com/problems/",
   "CSES": "https://cses.fi/problemset/task/",
 };
@@ -17,8 +20,8 @@ export class Problem {
     public source: string,
     public name: string,
     public id: string,
-    public starred?: boolean,
     public difficulty?: 'Intro' | 'Easy' | 'Normal' | 'Hard' | 'Very Hard',
+    public starred?: boolean,
     public tags?: string[],
     public sketch?: string,
   ) {
