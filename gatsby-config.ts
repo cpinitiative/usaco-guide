7ea4100498255aca514828e4d06498516142b3d6
Nathan Wang <nathan.r.wang@gmail.com>
2020-07-06 00:42:33 -0700

    add google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -69,6 +69,35 @@ export const plugins = [
       icon: `src/assets/logo-square.png`, // This path is relative to the root of the site.
     },
   },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      // The property ID; the tracking code won't be generated without it
+      trackingId: 'UA-55628264-7',
+      // Defines where to place the tracking script - `true` in the head and `false` in the body
+      head: false,
+      // Setting this parameter is optional
+      // anonymize: true,
+      // Setting this parameter is also optional
+      // respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      // Delays sending pageview hits on route update (in milliseconds)
+      pageTransitionDelay: 100,
+      // Enables Google Optimize using your container Id
+      // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+      // Enables Google Optimize Experiment ID
+      // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+      // Set Variation ID. 0 for original 1,2,3....
+      // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+      // Defers execution of google analytics script after page load
+      // defer: false,
+      // Any additional optional fields
+      // sampleRate: 5,
+      // siteSpeedSampleRate: 10,
+      // cookieDomain: "example.com",
+    },
+  },
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   // `gatsby-plugin-offline`,
