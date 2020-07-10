8548f3d68a08eb57a6146288f06d273d4f34849b
Nathan Wang <nathan.r.wang@gmail.com>
2020-07-10 14:32:58 -0700

    revert unordered list styles

--- src/components/markdown/MDXProvider.tsx
+++ src/components/markdown/MDXProvider.tsx
@@ -135,26 +135,6 @@ const components = {
   ),
 
   p: props => <p {...props} className="mb-4" />,
-  'ul.li': ({ children, ...props }) => (
-    <li {...props} className="flex">
-      <span className="mr-4">
-        <svg
-          fill="currentColor"
-          height="16"
-          width="16"
-          viewBox="0 0 512 512"
-          className="inline-block align-middle text-blue-600"
-        >
-          <path
-            d="M322.7,128.4L423,233.4c6,5.8,9,13.7,9,22.4c0,8.7-3,16.5-9,22.4L322.7,383.6c-11.9,12.5-31.3,12.5-43.2,0
-	c-11.9-12.5-11.9-32.7,0-45.2l48.2-50.4h-217C93.7,288,80,273.7,80,256c0-17.7,13.7-32,30.6-32h217l-48.2-50.4
-	c-11.9-12.5-11.9-32.7,0-45.2C291.4,115.9,310.7,115.9,322.7,128.4z"
-          />
-        </svg>
-      </span>
-      <div>{children}</div>
-    </li>
-  ),
 };
 
 export default function ({ children }) {
