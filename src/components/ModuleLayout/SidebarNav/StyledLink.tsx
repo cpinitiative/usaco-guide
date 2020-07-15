8c369d137a7ed65a553f46890600b24ea438033a
Nathan Wang <nathan.r.wang@gmail.com>
2020-07-15 00:46:02 -0700

    add styled link animation

--- src/components/ModuleLayout/SidebarNav/StyledLink.tsx
+++ src/components/ModuleLayout/SidebarNav/StyledLink.tsx
@@ -1,8 +1,18 @@
-import styled, { css } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import tw from 'twin.macro';
 import { Link } from 'gatsby';
 
 // Almost entirely copy pasted from https://gatsbyjs.org/ oops
+
+const showBar = keyframes`
+  from {
+    transform: translateX(-110px);
+  }
+
+  to {
+    transform: translateX(-92px);
+  }
+`;
 
 const StyledLink = styled.span`
   ${tw`focus:outline-none transition ease-in-out duration-150 hover:text-blue-700 focus:bg-blue-100 relative flex items-center px-6 py-3 text-sm leading-5`}
@@ -24,21 +34,22 @@ const StyledLink = styled.span`
     transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s;
   }
 
-  ${props =>
-    props.isActive &&
-    css`
-      &::after {
-        transform: ${props =>
-          props.isActive ? 'translateX(-92px)' : 'translateX(-100px)'};
-        width: 100px;
-        border-radius: 4px;
-        ${tw`bg-blue-600`}
-      }
-    `}
+  &::after {
+    transform: translateX(-110px);
+    width: 100px;
+    border-radius: 4px;
+    ${tw`bg-blue-600`}
+    ${({ isActive }) =>
+      isActive &&
+      css`
+        animation: ${showBar} 250ms forwards cubic-bezier(0.4, 0, 0.2, 1);
+      `}
+  }
 
   &::before {
-    transform: scale(0.1);
+    transform: ${({ isActive }) => (isActive ? 'scale(1)' : 'scale(0.1)')};
     border-radius: 100%;
+    ${({ isActive }) => isActive && tw`bg-blue-600`}
   }
 
   &:hover {
