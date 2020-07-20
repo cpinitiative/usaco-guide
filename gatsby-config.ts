4b4b78e0808463c00c01a6dca32031d66f706359
Nathan Wang <nathan.r.wang@gmail.com>
2020-07-20 01:00:04 -0700

    upd

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -121,7 +121,7 @@ export const plugins = [
       // cookieDomain: "example.com",
     },
   },
-  'gatsby-plugin-webpack-bundle-analyser-v2',
+  // 'gatsby-plugin-webpack-bundle-analyser-v2',
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   // `gatsby-plugin-offline`,
--- src/templates/syllabusTemplate.tsx
+++ src/templates/syllabusTemplate.tsx
@@ -89,8 +89,6 @@ export default function Template(props) {
     ['Skipped'],
     ['Not Attempted']
   );
-  // try to break travis
-  console.log(window.location.hostname);
 
   return (
     <Layout>
