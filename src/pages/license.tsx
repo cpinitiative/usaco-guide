24573c1fb2a6cee38f5e0a9d8db26c6b6bb63c4c
Nathan Wang <nathan.r.wang@gmail.com>
2020-07-20 16:26:56 -0700

    sentry works yay

--- src/pages/license.tsx
+++ src/pages/license.tsx
@@ -9,13 +9,6 @@ export default function LicensePage(props: PageProps) {
       <SEO title="License and Usage" />
 
       <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-        <button
-          onClick={() => {
-            throw 'Hello Sentry??';
-          }}
-        >
-          Break the app (and test sentry!)
-        </button>
         <Link to="/" className="block mt-4 underline text-blue-600">
           &larr; Back to Home
         </Link>
