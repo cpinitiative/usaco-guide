ce443cf44a5d9bc09ba4ae09bd81e6cc4fa4c8b8
Nathan Wang <nathan.r.wang@gmail.com>
2020-07-25 18:30:40 -0700

    upd search styling

--- src/components/TopNavigationBar.tsx
+++ src/components/TopNavigationBar.tsx
@@ -97,7 +97,7 @@ const ModuleSearch = ({ hits, currentRefinement, refine }) => {
                 to={moduleIDToURLMap[hit.id]}
                 className="block hover:bg-blue-100 px-4 py-2 transition duration-150 ease-in-out"
               >
-                <h3 className="text-lg text-gray-600 font-medium">
+                <h3 className="text-gray-600 font-medium">
                   <Highlight hit={hit} attribute="title" /> -{' '}
                   {SECTION_LABELS[hit.division]}
                 </h3>
