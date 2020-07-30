df7e556946991b4525d6c3cf983854967800f6a6
Benjamin Qi <bqi343@gmail.com>
2020-07-30 17:54:32 -0400

    break CF links

--- src/models/problem.ts
+++ src/models/problem.ts
@@ -1,30 +1,92 @@
-const sources = {
-  AC: 'https://atcoder.jp/',
-  CC: 'https://www.codechef.com/problems/',
-  CSA: 'https://csacademy.com/contest/archive/task/',
-  DMOJ: 'https://dmoj.ca/problem/',
-  SPOJ: 'https://www.spoj.com/problems/',
-  YS: 'https://judge.yosupo.jp/problem/',
-  CF: 'https://codeforces.com/',
-  Bronze: 'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
-  Silver: 'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
-  Gold: 'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
-  'Old Bronze': 'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
-  'Old Silver': 'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
-  'Old Gold': 'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
-  Plat: 'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
-  Kattis: 'https://open.kattis.com/problems/',
-  CSES: 'https://cses.fi/problemset/task/',
-  LC: 'https://leetcode.com/problems/',
-  ojuz: 'https://oj.uz/problem/view/',
-  HR: 'https://www.hackerrank.com/',
-  Infoarena: 'https://infoarena.ro/problema/',
+import { ProblemsList } from '../components/markdown/ProblemsList/ProblemsList';
+
+const contests = {
+  CCC: ['DMOJ', 'Canadian Computing Competition'],
+  CCO: ['DMOJ', 'Canadian Computing Olympiad'],
+  APIO: ['oj.uz', 'Asia-Pacific Informatics Olympiad'],
+  'Baltic OI': ['oj.uz', 'Baltic Olympiad in Informatics'],
+  CEOI: ['oj.uz', 'Central European Olympiad in Informatics'],
+  COCI: ['oj.uz', 'Croatian Open Contest in Informatics'],
+  IOI: ['oj.uz', 'International Olympiad in Informatics'],
+  IZhO: ['oj.uz', 'International Zhautykov Olympiad'],
+  JOI: ['oj.uz', 'Japanese Olympiad in Informatics'],
+  LMiO: ['oj.uz', 'Lithuanian Olympiad in Informatics'],
+  RMI: ['oj.uz', 'Romanian Master of Informatics'],
 };
 
-const oly = {
-  CCO: 'DMOJ',
-  IZhO: 'ojuz',
+const probSources = {
+  'Old Bronze': [
+    'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
+    'USACO Platinum did not exist prior to 2015-16.',
+  ],
+  'Old Gold': [
+    'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
+    'USACO Platinum did not exist prior to 2015-16.',
+  ],
+  'Old Silver': [
+    'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
+    'USACO Platinum did not exist prior to 2015-16.',
+  ],
+  Bronze: [
+    'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
+    'USACO 2015-16 to present',
+  ],
+  Silver: [
+    'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
+    'USACO 2015-16 to present',
+  ],
+  Gold: [
+    'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
+    'USACO 2015-16 to present',
+  ],
+  Plat: [
+    'http://www.usaco.org/index.php?page=viewproblem2&cpid=',
+    'USACO 2015-16 to present',
+  ],
+  AC: [
+    'https://atcoder.jp/',
+    'AtCoder',
+    'The editorial tab should be second from the right.',
+  ],
+  CC: ['https://www.codechef.com/problems/', 'CodeChef'],
+  CF: [
+    'https://codeforces.com/contest/',
+    'CodeForces',
+    'Check contest materials, located to the right of the problem statement.',
+  ],
+  CSA: [
+    'https://csacademy.com/contest/archive/task/',
+    'CS Academy',
+    'The editorial tab should be right next to the statement tab.',
+  ],
+  CSES: [
+    'https://cses.fi/problemset/task/',
+    'Code Submission Evaluation System (includes CPH problemset)',
+  ],
+  DMOJ: ['https://dmoj.ca/problem/', 'Don Mills Online Judge'],
+  HR: ['https://www.hackerrank.com/', 'HackerRank'],
+  InfoArena: ['https://infoarena.ro/', ''],
+  Kattis: ['https://open.kattis.com/problems/', 'open.kattis.com'],
+  LC: ['https://leetcode.com/problems/', 'LeetCode'],
+  POI: [
+    'https://szkopul.edu.pl/problemset/problem/',
+    'Polish Olympiad in Informatics',
+  ],
+  SPOJ: ['https://www.spoj.com/problems/', 'Sphere Online Judge'],
+  TLX: [
+    'https://tlx.toki.id/',
+    'tlx.toki.id',
+    'The editorial should be available in the announcements tab.',
+  ],
+  'oj.uz': ['https://oj.uz/problem/view/', ''],
+  ojuz: ['https://oj.uz/problem/view/', ''],
+  YS: ['https://judge.yosupo.jp/problem/', 'Library Checker'],
 };
+
+// if (sol == '' && problem.source == 'HE') {
+//   sol = '@Check HE';
+//   hover = 'The editorial tab should be right next to the problem tab.';
+// }
 
 export class Problem {
   public url: string;
@@ -38,6 +100,8 @@ export class Problem {
   public isIntro: boolean;
   public sketch: string | null = null;
   public solution: string | null = null;
+  public des: string = '';
+  public hover: string = '';
 
   get uniqueID() {
     return this.url;
@@ -65,6 +129,10 @@ export class Problem {
     sol?: string,
     public solQuality: 'bad' | 'ok' | 'good' = 'ok'
   ) {
+    if (!sol && source in probSources && probSources[source].length == 3) {
+      sol = '@Check ' + source;
+      this.hover = probSources[source][2];
+    }
     if (
       sol !== undefined &&
       (sol.startsWith('http') ||
@@ -78,21 +146,20 @@ export class Problem {
     this.isIntro = labels && labels.includes('Intro|');
     if (labels) labels = labels.replace('Intro|', '') as any;
     this.difficulty = labels as any;
-    if (!id.startsWith('http')) {
-      if (source in sources) {
-        if (source == 'CF' && /^[0-9]+[A-Z]$/.test(id)) {
-          id =
-            'contest/' +
-            id.substring(0, id.length - 1) +
-            '/problem/' +
-            id[id.length - 1];
-        }
-        this.id = id;
-        this.url = sources[source] + id;
-      } else if (source in oly) this.url = sources[oly[source]] + id;
-      else
+    this.url = id;
+    // console.log(this.url)
+    if (source in probSources) {
+      if (!this.url.startsWith('http'))
+        this.url = probSources[source][0] + this.url;
+      this.des = probSources[source][1];
+    } else if (source in contests) {
+      if (!this.url.startsWith('http'))
+        this.url = probSources[contests[source][0]][0] + this.url;
+      this.des = contests[source][1];
+    } else {
+      if (!this.url.startsWith('http'))
         throw `URL ${id} is not valid. Did you make a typo in the problem source (${source}), or in the URL? Problem name: ${name}`;
-    } else this.url = id;
+    }
   }
 }
 
