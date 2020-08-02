293386d43df9c15a121b138169155dd4202338e8
Nathan Wang <nathan.r.wang@gmail.com>
2020-08-02 16:37:21 -0700

    update dependencies; try to add admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -4,83 +4,9 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { useEffect, useState } from 'react';
 
-const babel = require(`@babel/core`);
-const objRestSpread = require(`@babel/plugin-proposal-object-rest-spread`);
-
-const mdxToJs = require(`gatsby-plugin-mdx/utils/mdx`);
-const htmlAttrToJSXAttr = require(`gatsby-plugin-mdx/utils/babel-plugin-html-attr-to-jsx-attr`);
-const removeExportKeywords = require(`gatsby-plugin-mdx/utils/babel-plugin-remove-export-keywords`);
-const BabelPluginPluckImports = require(`gatsby-plugin-mdx/utils/babel-plugin-pluck-imports`);
-
-import Markdown from '../components/markdown/Markdown';
-
-const renderMarkdown = markdown => {
-  const [body, setBody] = useState(null);
-  useEffect(() => {
-    (async () => {
-      try {
-        let code = await mdxToJs(markdown, {
-          defaultLayouts: {},
-          extensions: [`.mdx`],
-          mediaTypes: [`text/markdown`, `text/x-markdown`],
-          rehypePlugins: [require('rehype-katex')],
-          remarkPlugins: [
-            require('remark-external-links'),
-            require('remark-math'),
-          ],
-          plugins: [],
-          root: process.cwd(),
-          gatsbyRemarkPlugins: [],
-          globalScope: `export default {}`,
-          shouldBlockNodeFromTransformation: () => false,
-        });
-        const instance = new BabelPluginPluckImports();
-        const result = babel.transform(code, {
-          configFile: false,
-          plugins: [
-            instance.plugin,
-            objRestSpread,
-            htmlAttrToJSXAttr,
-            removeExportKeywords,
-          ],
-          presets: [
-            require(`@babel/preset-react`),
-            [
-              require(`@babel/preset-env`),
-              {
-                useBuiltIns: `entry`,
-                corejs: 2,
-                modules: false,
-              },
-            ],
-          ],
-        });
-
-        const identifiers = Array.from(instance.state.identifiers);
-        const imports = Array.from(instance.state.imports);
-        if (!identifiers.includes(`React`)) {
-          identifiers.push(`React`);
-          imports.push(`import * as React from 'react'`);
-        }
-
-        let body = result.code
-          .replace(
-            /export\s*default\s*function\s*MDXContent\s*/,
-            `return function MDXContent`
-          )
-          .replace(
-            /export\s*{\s*MDXContent\s+as\s+default\s*};?/,
-            `return MDXContent;`
-          );
-        console.log(body);
-      } catch (e) {
-        console.error('handling gracefully', e);
-      }
-    })();
-  }, [markdown]);
-  if (body === null) return null;
-  return <Markdown body={body} />;
-};
+const RawMarkdownRenderer = React.lazy(() =>
+  import('../components/AdminMarkdownRenderer')
+);
 
 export default function AdminPage(props: PageProps) {
   const [markdown, setMarkdown] = useState('');
@@ -99,7 +25,11 @@ export default function AdminPage(props: PageProps) {
           />
         </div>
         <div className="flex-1 p-4 h-screen overflow-y-auto">
-          {renderMarkdown(markdown)}
+          {typeof window !== 'undefined' && (
+            <React.Suspense fallback={'Loading'}>
+              <RawMarkdownRenderer markdown={markdown} />
+            </React.Suspense>
+          )}
         </div>
       </div>
     </Layout>
