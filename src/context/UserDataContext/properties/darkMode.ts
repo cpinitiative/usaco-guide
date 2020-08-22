cfb5a658fe4bbc7643629941b5b8690692018663
Nathan Wang <nathan.r.wang@gmail.com>
2020-08-21 22:04:00 -0700

    fix dark mode on localhost

--- src/context/UserDataContext/properties/darkMode.ts
+++ src/context/UserDataContext/properties/darkMode.ts
@@ -11,23 +11,24 @@ export default class DarkMode extends SimpleUserDataPropertyAPI {
   protected storageKey = 'darkMode';
   protected defaultValue = false;
   protected setterFunctionName = 'setDarkMode';
+  private _value = false;
 
-  public setTriggerRerenderFunction = (func: Function) => {
-    this.triggerRerender = () => {
-      if (this.value) {
-        document.documentElement.classList.add('mode-dark');
-      } else {
-        document.documentElement.classList.remove('mode-dark');
-      }
-      document.documentElement.classList.add('transitioning-color-scheme');
-      setTimeout(
-        () =>
-          document.documentElement.classList.remove(
-            'transitioning-color-scheme'
-          ),
-        0
-      );
-      func();
-    };
-  };
+  get value() {
+    return this._value;
+  }
+  set value(val: any) {
+    if (this._value === val) return;
+    this._value = val;
+    if (this.value) {
+      document.documentElement.classList.add('mode-dark');
+    } else {
+      document.documentElement.classList.remove('mode-dark');
+    }
+    document.documentElement.classList.add('transitioning-color-scheme');
+    setTimeout(
+      () =>
+        document.documentElement.classList.remove('transitioning-color-scheme'),
+      0
+    );
+  }
 }
