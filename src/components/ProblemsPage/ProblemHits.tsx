7d23afe8ebeba2a825c020e22ae2875c91020d5f
Nathan Wang <nathan.r.wang@gmail.com>
2020-09-06 18:37:17 -0700

    fix: remove duplicate cards in problem search, closes #232

--- src/components/ProblemsPage/ProblemHits.tsx
+++ src/components/ProblemsPage/ProblemHits.tsx
@@ -96,7 +96,7 @@ export default function ProblemHits({ hits }) {
   return (
     <div className="grid sm:p-4 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {hits.map(hit => (
-        <ProblemHit hit={hit} key={hit.name} />
+        <ProblemHit hit={hit} key={hit.objectID} />
       ))}
     </div>
   );
