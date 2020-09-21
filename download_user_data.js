9de234a25824ef0b178de6c7b9571e5b35810fce
Nathan Wang <nathan.r.wang@gmail.com>
2020-09-20 18:29:56 -0700

    feat: remove beta warning for signing in

--- download_user_data.js
+++ download_user_data.js
@@ -1,19 +1,45 @@
-// https://github.com/dalenguyen/firestore-import-export
-fs = require('fs');
-
-const firestoreService = require('firestore-export-import');
 const serviceAccount = require('./service.json');
+const admin = require('firebase-admin');
+const fs = require('fs');
 
-// Initiate Firebase App
-// appName is optional, you can omit it.
-const appName = '[DEFAULT]';
-const databaseURL = 'https://usaco-guide.firebaseio.com';
-firestoreService.initializeApp(serviceAccount, databaseURL, appName);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// Start exporting your data
-firestoreService.backup('users').then(data =>
-  fs.writeFile('user_data.json', JSON.stringify(data), function (err) {
-    if (err) return console.log(err);
-    console.log('OK');
+const db = admin.firestore();
+
+admin
+  .auth()
+  .listUsers(1000)
+  .then(function (listUsersResult) {
+    let data = {};
+    listUsersResult.users.forEach(function (userRecord) {
+      data[userRecord.uid] = userRecord.toJSON();
+    });
+    fs.writeFile('auth_data.json', JSON.stringify(data), function (err) {
+      if (err) return console.log(err);
+      console.log('OK');
+    });
   })
-);
+  .catch(function (error) {
+    console.log('Error listing users:', error);
+  });
+
+db.collection('users')
+  .get()
+  .then(snapshot => {
+    let data = {};
+    snapshot.forEach(doc => {
+      data[doc.id] = doc.data();
+    });
+    return data;
+  })
+  .then(data => {
+    fs.writeFile('user_data.json', JSON.stringify(data), function (err) {
+      if (err) return console.log(err);
+      console.log('OK');
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
