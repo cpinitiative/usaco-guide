d2beda57376a9f754a80a7b18c5d9fc47840ba58
Benjamin Qi <bqi343@gmail.com>
2020-09-30 22:48:53 -0400

    add fileRelativePath as part of Solution / Module info

--- src/components/MarkdownLayout/TableOfContents/getGithubUrl.tsx
+++ src/components/MarkdownLayout/TableOfContents/getGithubUrl.tsx
@@ -1,47 +1,21 @@
 import MarkdownLayoutContext from '../../../context/MarkdownLayoutContext';
 import { useContext, useRef, useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import { SolutionInfo } from '../../../models/solution';
+import { ModuleInfo } from '../../../models/module';
 
 export default function getGithubUrl() {
   const moduleLayoutInfo = useContext(MarkdownLayoutContext);
-  const id = moduleLayoutInfo.markdownLayoutInfo.id;
-  const data = useStaticQuery(graphql`
-    query {
-      allMdx {
-        edges {
-          node {
-            frontmatter {
-              id
-            }
-            fields {
-              division
-            }
-            parent {
-              ... on File {
-                name
-                relativePath
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
-  const edges = data['allMdx']['edges'];
-  let relativePath = '';
-  for (let i = 0; i < edges.length; ++i) {
-    const node = edges[i]['node'];
-    if (node['frontmatter']['id'] == id) {
-      relativePath = node['parent']['relativePath'];
-    }
-  }
+  const markdownInfo = moduleLayoutInfo.markdownLayoutInfo;
+  const relativePath = markdownInfo.fileRelativePath;
   let githubUrl = 'https://github.com/cpinitiative/usaco-guide/blob/master/';
-  if (relativePath == '') {
-  } else if (/\d_.*/.test(relativePath)) {
+
+  if (markdownInfo instanceof ModuleInfo) {
+    // /\d_.*/.test(relativePath)
     githubUrl += 'content/' + relativePath;
   } else {
     githubUrl += 'solutions/' + relativePath;
   }
-  githubUrl = githubUrl.replace(' ', '%20');
+  githubUrl = githubUrl.replace(' ', '%20'); // is there some function that can automatically escape chars?
   return githubUrl;
 }
