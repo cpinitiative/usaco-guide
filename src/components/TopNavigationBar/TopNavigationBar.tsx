81cea0366110f61b945be8dc0189f94e014ce3eb
jiaxuan <4152386+koxuan@users.noreply.github.com>
2020-10-19 22:11:07 +0800

    dropdown menu ui added

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -309,6 +309,76 @@ export default function TopNavigationBar({
             <div className="hidden lg:ml-4 lg:flex lg:items-center">
               <div className="flex-shrink-0">
                 <UserAuthButton className="relative inline-flex items-center px-2 py-1 border border-transparent text-base leading-6 font-medium rounded-md text-gray-500 hover:text-gray-700 dark:text-dark-high-emphasis focus:outline-none focus:shadow-outline-blue transition ease-in-out duration-150" />
+
+                <div className="relative inline-block text-left">
+                  <div>
+                    <span className="rounded-md shadow-sm">
+                      <button
+                        type="button"
+                        className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"
+                        id="options-menu"
+                        aria-haspopup="true"
+                        aria-expanded="true"
+                      >
+                        Options
+                        <svg
+                          className="-mr-1 ml-2 h-5 w-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                    </span>
+                  </div>
+
+                  <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
+                    <div className="rounded-md bg-white shadow-xs">
+                      <div
+                        className="py-1"
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="options-menu"
+                      >
+                        <a
+                          href="#"
+                          className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                          role="menuitem"
+                        >
+                          Account settings
+                        </a>
+                        <a
+                          href="#"
+                          className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                          role="menuitem"
+                        >
+                          Support
+                        </a>
+                        <a
+                          href="#"
+                          className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                          role="menuitem"
+                        >
+                          License
+                        </a>
+                        <form method="POST" action="#">
+                          <button
+                            type="submit"
+                            className="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            role="menuitem"
+                          >
+                            Sign out
+                          </button>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
