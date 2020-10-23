38ff55ee719f22446986e4bde3a3349231ed71dd
Mrinall <bossguy124567@gmail.com>
2020-10-23 19:51:21 -0400

    DONE

--- src/components/MarkdownLayout/MarkCompleteButton.tsx
+++ src/components/MarkdownLayout/MarkCompleteButton.tsx
@@ -1,15 +1,6 @@
 import * as React from 'react';
 import Transition from '../Transition';
 import { ModuleProgressOptions } from '../../models/module';
-import Button from '@material-ui/core/Button';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import DoneIcon from '@material-ui/icons/Done';
-import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
-import StarIcon from '@material-ui/icons/Star';
-import BookmarkIcon from '@material-ui/icons/Book';
-import { blue, blueGrey, green, yellow } from '@material-ui/core/colors';
-
 const MarkCompleteButton = ({
   state,
   onChange,
@@ -25,15 +16,7 @@ const MarkCompleteButton = ({
     onChange(option);
   };
   const ref = React.useRef();
-  makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        '& > *': {
-          margin: theme.spacing(1),
-        },
-      },
-    })
-  );
+
   React.useEffect(() => {
     const handleClick = e => {
       // @ts-ignore
@@ -46,16 +29,59 @@ const MarkCompleteButton = ({
   const icon = (status: string) => {
     switch (status) {
       case 'Reading':
-        return <MenuBookIcon style={{ color: blue[500] }} fontSize="small" />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5 text-yellow-300"
+          >
+            <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
+          </svg>
+        );
       case 'Complete':
-        return <DoneIcon style={{ color: green[500] }} fontSize="small" />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5 text-green-400"
+          >
+            <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
+          </svg>
+        );
       case 'Practicing':
-        return <FitnessCenterIcon fontSize="small" />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5 text-orange-500"
+          >
+            <path d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" />
+          </svg>
+        );
       case 'Skipped':
-        return <StarIcon fontSize="small" style={{ color: yellow[700] }} />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5 text-blue-400"
+          >
+            <path d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" />
+          </svg>
+        );
       case 'Ignored':
         return (
-          <BookmarkIcon fontSize="small" style={{ color: blueGrey[700] }} />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5 text-purple-400"
+          >
+            <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+          </svg>
         );
     }
   };
@@ -63,7 +89,7 @@ const MarkCompleteButton = ({
     <div className="relative inline-block text-left" ref={ref}>
       <div>
         <span className="rounded-md shadow-sm">
-          <Button
+          <button
             type="button"
             className="inline-flex justify-center w-full rounded-md border border-gray-300 dark:border-gray-800 px-4 py-2 bg-white dark:bg-gray-900 text-sm leading-5 font-medium text-gray-700 dark:text-dark-high-emphasis hover:text-gray-500 dark-hover:text-dark-high-emphasis focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150"
             id="options-menu"
@@ -83,7 +109,7 @@ const MarkCompleteButton = ({
                 clipRule="evenodd"
               />
             </svg>
-          </Button>
+          </button>
         </span>
       </div>
 
@@ -111,15 +137,15 @@ const MarkCompleteButton = ({
               aria-labelledby="options-menu"
             >
               {ModuleProgressOptions.map(option => (
-                <Button
-                  className="inline-flex justify-center w-full rounded-md border border-gray-300 dark:border-gray-800 px-4 py-2 bg-white dark:bg-gray-900 text-sm leading-5 font-medium text-gray-700 dark:text-dark-high-emphasis hover:text-gray-500 dark-hover:text-dark-high-emphasis focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150"
-                  size="small"
+                <button
                   key={option}
                   onClick={() => handleSelect(option)}
+                  className="flex items-center w-full text-left px-3 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
                   role="menuitem"
                 >
-                  {icon(option)} &nbsp;&nbsp;{option}
-                </Button>
+                  <span className="mr-2">{icon(option)} </span>{' '}
+                  <span className="flex-1">{option}</span>
+                </button>
               ))}
             </div>
           </div>
