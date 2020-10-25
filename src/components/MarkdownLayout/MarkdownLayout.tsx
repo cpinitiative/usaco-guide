bf53bd37d0faa5a8a773030e7b97787c8651d48e
Nathan Wang <nathan.r.wang@gmail.com>
2020-10-25 14:30:47 -0700

    fix: add dark mode support to sign in panel

--- src/components/MarkdownLayout/MarkdownLayout.tsx
+++ src/components/MarkdownLayout/MarkdownLayout.tsx
@@ -484,12 +484,12 @@ export default function MarkdownLayout({
 
                 {!firebaseUser && (
                   <>
-                    <div className="bg-gray-50 sm:rounded-lg">
+                    <div className="bg-gray-50 dark:bg-gray-900 sm:rounded-lg">
                       <div className="px-4 py-5 sm:p-6">
-                        <h3 className="text-lg leading-6 font-medium text-gray-900">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-dark-high-emphasis">
                           You're not signed in!
                         </h3>
-                        <div className="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+                        <div className="mt-2 max-w-xl text-sm leading-5 text-gray-500 dark:text-dark-med-emphasis">
                           <p>
                             Sign in to save your progress and sync your settings
                             across devices.
@@ -500,7 +500,7 @@ export default function MarkdownLayout({
                             <button
                               type="button"
                               onClick={() => signIn()}
-                              className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+                              className="inline-flex items-center px-4 py-2 border border-gray-300 dark:border-gray-700 text-sm leading-5 font-medium rounded-md text-gray-700 dark:text-dark-high-emphasis bg-white dark:bg-gray-800 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
                             >
                               Sign in
                             </button>
