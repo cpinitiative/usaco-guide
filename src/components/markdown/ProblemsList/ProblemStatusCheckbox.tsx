6ca89f403992f4582b03d525f32fc974c6b469da
Mrinall <bossguy124567@gmail.com>
2020-11-10 13:32:20 -0500

    Remove bad method

--- src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
+++ src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
@@ -14,90 +14,6 @@ const StyledTippy = styled(Tippy)`
     padding: 0;
   }
 `;
-const Chooseicon = status => {
-  switch (status) {
-    case 'Reading':
-      return (
-        <span className="mr-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5 text-yellow-300"
-          >
-            <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
-          </svg>
-        </span>
-      );
-    case 'Complete':
-      return (
-        <span className="mr-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5 text-green-400"
-          >
-            <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
-          </svg>
-        </span>
-      );
-    case 'Practicing':
-      return (
-        <span className="mr-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5 text-orange-500"
-          >
-            <path d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" />
-          </svg>
-        </span>
-      );
-    case 'Skipped':
-      return (
-        <span className="mr-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5 text-blue-400"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-              clipRule="evenodd"
-            />
-            <path
-              fillRule="evenodd"
-              d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </span>
-      );
-    case 'Ignored':
-      return (
-        <span className="mr-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5 text-purple-400"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </span>
-      );
-    default:
-      return;
-  }
-};
 
 const ProgressDropdown = ({ onProgressSelected, currentProgress }) => {
   const [activeProgress, setActiveProgress] = useState<ProblemProgress>();
@@ -123,7 +39,6 @@ const ProgressDropdown = ({ onProgressSelected, currentProgress }) => {
               progress === status ? 'font-semibold' : 'font-normal'
             } block truncate`}
           >
-            {Chooseicon(progress)}
             {progress}
           </span>
 
