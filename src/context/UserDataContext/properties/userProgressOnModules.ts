e13713f61825b9fc24dd2c631b55dd5e87bfb678
Nathan Wang <nathan.r.wang@gmail.com>
2020-11-19 17:29:38 -0800

    feat: add activity to database

--- src/context/UserDataContext/properties/userProgressOnModules.ts
+++ src/context/UserDataContext/properties/userProgressOnModules.ts
@@ -1,21 +1,19 @@
 import UserDataPropertyAPI from '../userDataPropertyAPI';
 import { ModuleProgress } from '../../../models/module';
+import { ModuleActivity } from '../../../models/activity';
 
 export type UserProgressOnModulesAPI = {
   userProgressOnModules: { [key: string]: ModuleProgress };
+  userProgressOnModulesActivity: ModuleActivity[];
   setModuleProgress: (moduleID: string, progress: ModuleProgress) => void;
-
-  // userProgressOnProblems: { [key: string]: ProblemProgress };
-  // setUserProgressOnProblems: (
-  //   problem: Problem,
-  //   status: ProblemProgress
-  // ) => void;
 };
 
 export default class UserProgressOnModulesProperty extends UserDataPropertyAPI {
-  protected defaultValue = {};
-  protected storageKey = 'userProgressOnModules';
-  private value = {};
+  private progressStorageKey = 'userProgressOnModules';
+  private progressValue = {};
+
+  private activityStorageKey = 'userProgressOnModulesActivity';
+  private activityValue: ModuleActivity[] = [];
 
   initializeFromLocalStorage = () => {
     let legacyValue = this.getValueFromLocalStorage(
@@ -24,57 +22,85 @@ export default class UserProgressOnModulesProperty extends UserDataPropertyAPI {
     );
     if (legacyValue !== null) {
       window.localStorage.removeItem('guide:userData:progress');
-      this.value = legacyValue;
+      this.progressValue = legacyValue;
       this.writeValueToLocalStorage();
     } else {
-      this.value = this.getValueFromLocalStorage(
-        this.getLocalStorageKey(this.storageKey),
-        this.defaultValue
+      this.progressValue = this.getValueFromLocalStorage(
+        this.getLocalStorageKey(this.progressStorageKey),
+        {}
       );
     }
+
+    this.activityValue = this.getValueFromLocalStorage(
+      this.getLocalStorageKey(this.activityStorageKey),
+      []
+    );
   };
 
   writeValueToLocalStorage = () => {
     this.saveLocalStorageValue(
-      this.getLocalStorageKey(this.storageKey),
-      this.value
+      this.getLocalStorageKey(this.progressStorageKey),
+      this.progressValue
+    );
+    this.saveLocalStorageValue(
+      this.getLocalStorageKey(this.activityStorageKey),
+      this.activityValue
     );
   };
 
   eraseFromLocalStorage = () => {
-    window.localStorage.removeItem(this.getLocalStorageKey(this.storageKey));
+    window.localStorage.removeItem(
+      this.getLocalStorageKey(this.progressStorageKey)
+    );
+    window.localStorage.removeItem(
+      this.getLocalStorageKey(this.activityStorageKey)
+    );
   };
 
   exportValue = (): any => {
     return {
-      [this.storageKey]: this.value,
+      [this.progressStorageKey]: this.progressValue,
+      [this.activityStorageKey]: this.activityValue,
     };
   };
 
   importValueFromObject = (data: object) => {
-    this.value = data['userProgressOnModules'] || this.defaultValue;
+    this.progressValue = data[this.progressStorageKey] || {};
+    this.activityValue = data[this.activityStorageKey] || [];
   };
 
-  getAPI = () => {
+  getAPI: () => UserProgressOnModulesAPI = () => {
     return {
-      userProgressOnModules: this.value,
+      userProgressOnModules: this.progressValue,
+      userProgressOnModulesActivity: this.activityValue,
+
       setModuleProgress: (moduleID: string, progress: ModuleProgress) => {
-        if (this.firebaseUserDoc) {
-          this.firebaseUserDoc.set(
-            {
-              [this.storageKey]: {
-                [moduleID]: progress,
-              },
-            },
-            { merge: true }
-          );
-        } else {
+        if (!this.firebaseUserDoc) {
           // if the user isn't using firebase, it is possible that they
           // have multiple tabs open, which can result in localStorage
           // being out of sync.
           this.initializeFromLocalStorage();
         }
-        this.value[moduleID] = progress;
+
+        this.activityValue.push({
+          timestamp: Date.now(),
+          moduleID: moduleID,
+          moduleProgress: progress,
+        });
+        this.progressValue[moduleID] = progress;
+
+        if (this.firebaseUserDoc) {
+          this.firebaseUserDoc.set(
+            {
+              [this.progressStorageKey]: {
+                [moduleID]: progress,
+              },
+              [this.activityStorageKey]: this.activityValue,
+            },
+            { merge: true }
+          );
+        }
+
         this.writeValueToLocalStorage();
         this.triggerRerender();
       },
