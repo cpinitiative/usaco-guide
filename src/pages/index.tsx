16ef5771379149b3ffa1266f655640ce934e5c7a
Nathan Wang <nathan.r.wang@gmail.com>
2020-11-28 00:02:01 -0800

    feat: add usage stats to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { useRef } from 'react';
 import Video from '../components/Video';
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import AuthorsSection from '../components/Index/AuthorsSection';
+import TrustedBy from '../components/Index/TrustedBy';
 
 export default function IndexPage(props: PageProps) {
   const learnMoreRef = useRef();
@@ -25,49 +26,6 @@ export default function IndexPage(props: PageProps) {
 
       {/* Begin Hero */}
       <div className="relative bg-white dark:bg-gray-900 overflow-hidden">
-        <div className="hidden lg:block lg:absolute lg:inset-0">
-          <svg
-            className="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8"
-            width="640"
-            height="784"
-            fill="none"
-            viewBox="0 0 640 784"
-          >
-            <defs>
-              <pattern
-                id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047"
-                x="118"
-                y="0"
-                width="20"
-                height="20"
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x="0"
-                  y="0"
-                  width="4"
-                  height="4"
-                  className="text-gray-200 dark:text-gray-800"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect
-              y="0"
-              width="640"
-              height="640"
-              className="text-gray-50 dark:text-gray-900"
-              fill="currentColor"
-            />
-            <rect
-              x="118"
-              width="404"
-              height="784"
-              fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)"
-            />
-          </svg>
-        </div>
-
         <div className="relative pt-6 pb-16 md:pb-20 lg:pb-24 xl:pb-32">
           <main className="mt-8 mx-auto max-w-6xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
             <div className="lg:grid lg:grid-cols-12 lg:gap-8">
@@ -120,43 +78,14 @@ export default function IndexPage(props: PageProps) {
 
               <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
                 <svg
-                  className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden"
-                  width="640"
-                  height="784"
-                  fill="none"
-                  viewBox="0 0 640 784"
+                  viewBox="0 0 200 200"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="invisible lg:visible absolute transform translate-x-32 text-blue-50 dark:text-gray-800"
                 >
-                  <defs>
-                    <pattern
-                      id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e"
-                      x="118"
-                      y="0"
-                      width="20"
-                      height="20"
-                      patternUnits="userSpaceOnUse"
-                    >
-                      <rect
-                        x="0"
-                        y="0"
-                        width="4"
-                        height="4"
-                        className="text-gray-200 dark:text-gray-800"
-                        fill="currentColor"
-                      />
-                    </pattern>
-                  </defs>
-                  <rect
-                    y="72"
-                    width="640"
-                    height="640"
-                    className="text-gray-50 dark:text-gray-900"
+                  <path
                     fill="currentColor"
-                  />
-                  <rect
-                    x="118"
-                    width="404"
-                    height="784"
-                    fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)"
+                    d="M60,-32.3C73.6,-11,77.7,18.1,66.1,39.4C54.5,60.8,27.3,74.4,1.3,73.7C-24.7,72.9,-49.5,57.9,-57,38.9C-64.5,19.9,-54.7,-3,-42.3,-23.5C-29.9,-44.1,-15,-62.3,4.1,-64.6C23.2,-67,46.3,-53.6,60,-32.3Z"
+                    transform="translate(100 100)"
                   />
                 </svg>
                 <Video />
@@ -166,6 +95,8 @@ export default function IndexPage(props: PageProps) {
         </div>
       </div>
       {/* End Hero */}
+
+      <TrustedBy />
 
       <div
         className="py-12 dark:bg-dark-surface"
@@ -474,16 +405,15 @@ export default function IndexPage(props: PageProps) {
                     <p className="text-base leading-6 text-gray-500 dark:text-gray-400">
                       If you get stuck, feel free to ask for help by clicking
                       the "Contact Us" button. It may take some time for your
-                      question to be addressed. 
-                      You can also head over to the{' '}
+                      question to be addressed. You can also head over to the{' '}
                       <OutboundLink
                         href="https://forum.usaco.guide"
                         target="_blank"
                         className="text-blue-600 dark:text-blue-400 underline"
                       >
-                        USACO Forum  
-                      </OutboundLink>
-                      {' '}for help.
+                        USACO Forum
+                      </OutboundLink>{' '}
+                      for help.
                     </p>
                   </dd>
                 </div>
