f07bfc0678a051e5051946ca4eb6548989de3f37
Nathan Wang <nathan.r.wang@gmail.com>
2020-12-02 20:34:37 -0800

    fix: fix tests

--- src/pages/__tests__/liveupdate.js
+++ src/pages/__tests__/liveupdate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import LiveUpdate from '../LiveUpdate';
+import LiveUpdate from '../liveupdate';
 
 jest.mock('../../components/layout', () => {
   return jest.fn(({ children }) => (
