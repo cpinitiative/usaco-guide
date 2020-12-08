8e5877efc87d67a1724d3aebb89b895ddf2a9c8c
Mrinall <bossguy124567@gmail.com>
2020-12-08 18:25:25 -0500

    Added some forms

--- src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
+++ src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
@@ -211,35 +211,12 @@ export default function ProblemStatusCheckbox({
                   <form action="#" method="POST">
                     <div className="shadow sm:rounded-md sm:overflow-hidden">
                       <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                        <div className="grid grid-cols-3 gap-6">
-                          <div className="col-span-3 sm:col-span-2">
-                            <label
-                              htmlFor="company_website"
-                              className="block text-sm font-medium text-gray-700"
-                            >
-                              Website
-                            </label>
-                            <div className="mt-1 flex rounded-md shadow-sm">
-                              <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                                http://
-                              </span>
-                              <input
-                                type="text"
-                                name="company_website"
-                                id="company_website"
-                                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                                placeholder="www.example.com"
-                              />
-                            </div>
-                          </div>
-                        </div>
-
                         <div>
                           <label
                             htmlFor="about"
                             className="block text-sm font-medium text-gray-700"
                           >
-                            About
+                            General Feedback
                           </label>
                           <div className="mt-1">
                             <textarea
@@ -247,82 +224,52 @@ export default function ProblemStatusCheckbox({
                               name="about"
                               rows="3"
                               className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                              placeholder="you@example.com"
+                              placeholder="What do you think about this problem?"
                             >
                               {' '}
                             </textarea>
                           </div>
                           <p className="mt-2 text-sm text-gray-500">
-                            Brief description for your profile. URLs are
-                            hyperlinked.
+                            This is a survey to collect user input on the
+                            provided problem
                           </p>
                         </div>
-
-                        <div>
-                          <label className="block text-sm font-medium text-gray-700">
-                            Photo
-                          </label>
-                          <div className="mt-2 flex items-center">
-                            <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                              <svg
-                                className="h-full w-full text-gray-300"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                              >
-                                <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                              </svg>
-                            </span>
-                            <button
-                              type="button"
-                              className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                              Change
-                            </button>
-                          </div>
-                        </div>
-
-                        <div>
-                          <label className="block text-sm font-medium text-gray-700">
-                            Cover photo
-                          </label>
-                          <div className="mt-2 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                            <div className="space-y-1 text-center">
-                              <svg
-                                className="mx-auto h-12 w-12 text-gray-400"
-                                stroke="currentColor"
-                                fill="none"
-                                viewBox="0 0 48 48"
-                                aria-hidden="true"
-                              >
-                                <path
-                                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                  stroke-width="2"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                />
-                              </svg>
-                              <div className="flex text-sm text-gray-600">
-                                <label
-                                  htmlFor="file-upload"
-                                  className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                                >
-                                  <span>Upload a file</span>
-                                  <input
-                                    id="file-upload"
-                                    name="file-upload"
-                                    type="file"
-                                    className="sr-only"
-                                  />
-                                </label>
-                                <p className="pl-1">or drag and drop</p>
-                              </div>
-                              <p className="text-xs text-gray-500">
-                                PNG, JPG, GIF up to 10MB
-                              </p>
-                            </div>
+                        <div className="mt-4">
+                          <span className="text-gray-700">
+                            Problem Difficulty
+                          </span>
+                          <div className="mt-2">
+                            <label className="inline-flex items-center">
+                              <input
+                                type="radio"
+                                className="form-radio"
+                                name="accountType"
+                                value="personal"
+                              />
+                              <span className="ml-2">Easy</span>
+                            </label>
+                            <label className="inline-flex items-center ml-6">
+                              <input
+                                type="radio"
+                                className="form-radio"
+                                name="accountType"
+                                value="busines"
+                              />
+                              <span className="ml-2">Medium</span>
+                            </label>
+                            <label className="inline-flex items-center ml-6">
+                              <input
+                                type="radio"
+                                className="form-radio"
+                                name="accountType"
+                                value="busines"
+                              />
+                              <span className="ml-2">Hard</span>
+                            </label>
                           </div>
                         </div>
                       </div>
+
                       <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                         <button
                           type="submit"
