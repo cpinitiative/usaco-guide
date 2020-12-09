f7932188814bca8dade8220ce8968ad425a2b928
Mrinall <bossguy124567@gmail.com>
2020-12-09 07:54:14 -0500

    Added difficulty support

--- src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
+++ src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
@@ -131,7 +131,9 @@ export default function ProblemStatusCheckbox({
   );
   const [solutionCode, setsolutionCode] = React.useState('No feedback entered');
   const firebase = useContext(FirebaseContext);
-
+  const handleDifficultyChange = difficulty => {
+    setdifficultyfeedback(difficulty);
+  };
   const handleSumbit = (event, problem) => {
     event.preventDefault();
 
@@ -229,10 +231,6 @@ export default function ProblemStatusCheckbox({
                               {' '}
                             </textarea>
                           </div>
-                          <p className="mt-2 text-sm text-gray-500">
-                            This is a survey to collect user input on the
-                            provided problem
-                          </p>
                         </div>
                         <div className="mt-4">
                           <span className="text-gray-700">
@@ -245,6 +243,9 @@ export default function ProblemStatusCheckbox({
                                 className="form-radio"
                                 name="accountType"
                                 value="personal"
+                                onClick={event =>
+                                  handleDifficultyChange('Easy')
+                                }
                               />
                               <span className="ml-2">Easy</span>
                             </label>
@@ -253,7 +254,10 @@ export default function ProblemStatusCheckbox({
                                 type="radio"
                                 className="form-radio"
                                 name="accountType"
-                                value="busines"
+                                value="Medium"
+                                onClick={event =>
+                                  handleDifficultyChange('Medium')
+                                }
                               />
                               <span className="ml-2">Medium</span>
                             </label>
@@ -262,12 +266,38 @@ export default function ProblemStatusCheckbox({
                                 type="radio"
                                 className="form-radio"
                                 name="accountType"
-                                value="busines"
+                                value="Hard"
+                                onClick={event =>
+                                  handleDifficultyChange('Hard')
+                                }
                               />
                               <span className="ml-2">Hard</span>
                             </label>
                           </div>
                         </div>
+                        <div>
+                          <label
+                            htmlFor="about"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Solution Code
+                          </label>
+                          <div className="mt-1">
+                            <textarea
+                              id="about"
+                              name="about"
+                              rows="3"
+                              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
+                              placeholder="What do you think about this problem?"
+                            >
+                              {' '}
+                            </textarea>
+                          </div>
+                        </div>
+                        <p className="mt-2 text-sm text-gray-500">
+                          This is a survey to collect user input on the provided
+                          problem
+                        </p>
                       </div>
 
                       <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
