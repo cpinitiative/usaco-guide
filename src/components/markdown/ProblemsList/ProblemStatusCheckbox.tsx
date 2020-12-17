e50ac05534c4809b9ec36e29d238aa95a9a3ec3a
Mrinall <bossguy124567@gmail.com>
2020-12-17 15:29:00 -0500

    readded the icons

--- src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
+++ src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
@@ -29,56 +29,110 @@ const ProgressDropdown = ({
   openModal,
 }) => {
   const [activeProgress, setActiveProgress] = useState<ProblemProgress>();
-
-  return (
-    <div>
-      <ul
-        tabIndex={-1}
-        className="max-h-60 rounded-md py-1 text-base leading-6 overflow-auto focus:outline-none sm:text-sm sm:leading-5 no-markdown"
+  const icon = (status: ProblemProgress, equal: Boolean) => {
+    const colorMap: { [key in ProblemProgress]: string } = {
+      'Not Attempted': '',
+      Solving: 'text-orange-500',
+      Solved: 'text-green-400',
+      Reviewing: 'text-red-500',
+      Skipped: 'text-blue-400',
+      Ignored: 'text-purple-400',
+    };
+    const pathMap: { [key in ProblemProgress]: JSX.Element } = {
+      'Not Attempted': <> </>,
+      Solving: (
+        <path d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" />
+      ),
+      Solved: (
+        <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" />
+      ),
+      Reviewing: (
+        <path
+          fill-rule="evenodd"
+          d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+          clip-rule="evenodd"
+        />
+      ),
+      Skipped: (
+        <>
+          <path
+            fillRule="evenodd"
+            d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+          <path
+            fillRule="evenodd"
+            d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+        </>
+      ),
+      Ignored: (
+        <path
+          fillRule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          clipRule="evenodd"
+        />
+      ),
+    };
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className={`h-5 w-5 ${equal ? 'text-white' : colorMap[status]}`}
       >
-        {PROBLEM_PROGRESS_OPTIONS.map(progress => (
-          <li
-            role="option"
+        {pathMap[status]}
+      </svg>
+    );
+  };
+  return (
+    <ul
+      tabIndex={-1}
+      className="rounded-md py-1 text-base leading-6 overflow-auto focus:outline-none sm:text-sm sm:leading-5 no-markdown"
+    >
+      {PROBLEM_PROGRESS_OPTIONS.map(progress => (
+        <li
+          role="option"
+          className={`${
+            activeProgress === progress
+              ? 'text-white bg-indigo-600'
+              : 'text-gray-900'
+          } cursor-default select-none relative py-2 pl-10 pr-4`}
+          key={progress}
+          onClick={() => onProgressSelected(progress)}
+          onMouseEnter={() => setActiveProgress(progress)}
+        >
+          <span
             className={`${
-              activeProgress === progress
-                ? 'text-white bg-indigo-600'
-                : 'text-gray-900'
-            } cursor-default select-none relative py-2 pl-10 pr-4`}
-            key={progress}
-            onClick={() => onProgressSelected(progress)}
-            onMouseEnter={() => setActiveProgress(progress)}
+              progress === status ? 'font-semibold' : 'font-normal'
+            } block truncate`}
           >
-            <span
-              className={`${
-                progress === status ? 'font-semibold' : 'font-normal'
-              } block truncate`}
-            >
-              {progress}
-            </span>
+            {progress}
+          </span>
 
-            {progress === currentProgress && (
-              <span
-                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                  activeProgress === progress ? 'text-white' : 'text-indigo-600'
-                }`}
-              >
-                <svg
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
+          <span className={`absolute inset-y-0 left-0 flex items-center pl-3`}>
+            {icon(progress, activeProgress === progress)}
+          </span>
+          {/*
+          {progress === currentProgress && (
+            <span
+              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                activeProgress === progress ? 'text-white' : 'text-indigo-600'
+              }`}
+            >
+              <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          )} */}
+        </li>
+      ))}
+    </ul>
   );
 };
 
