c4bf7a0b6688c5eb68431712ed79fd214782079c
Mrinall <bossguy124567@gmail.com>
2020-12-17 19:10:43 -0500

    Added public/private toggle

--- src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
+++ src/components/markdown/ProblemsList/ProblemStatusCheckbox.tsx
@@ -273,28 +273,6 @@ export default function ProblemStatusCheckbox({
                   <form action="#" method="POST">
                     <div className="shadow sm:rounded-md sm:overflow-hidden">
                       <div className="px-100 py-100 bg-white space-y-12 sm:p-6">
-                        <div>
-                          <label
-                            htmlFor="about"
-                            className="block text-sm font-medium text-gray-700"
-                          >
-                            General Feedback
-                          </label>
-                          <div className="mt-1">
-                            <textarea
-                              id="about"
-                              name="about"
-                              rows="3"
-                              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                              placeholder="What do you think about this problem?"
-                              onChange={event =>
-                                setTextFeedback(event.target.value)
-                              }
-                            >
-                              {' '}
-                            </textarea>
-                          </div>
-                        </div>
                         <div className="mt-4">
                           <span className="text-gray-700">
                             Problem Difficulty
@@ -335,6 +313,34 @@ export default function ProblemStatusCheckbox({
                                 }
                               />
                               <span className="ml-2">Hard</span>
+                            </label>
+                          </div>
+                        </div>
+                        <div className="mt-4">
+                          <span className="text-gray-700">
+                            Make feedback Public or Private
+                          </span>
+                          <div className="mt-2">
+                            <label className="inline-flex items-center">
+                              <input
+                                type="radio"
+                                className="form-radio"
+                                name="setStatus"
+                                value="setStatus"
+                                onClick={event => setPrivate(true)}
+                              />
+                              <span className="ml-2">Public</span>
+                            </label>
+
+                            <label className="inline-flex items-center ml-6">
+                              <input
+                                type="radio"
+                                className="form-radio"
+                                name="setStatus"
+                                value="setStatus"
+                                onClick={event => setPrivate(false)}
+                              />
+                              <span className="ml-2">Private</span>
                             </label>
                           </div>
                         </div>
