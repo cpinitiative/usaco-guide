9f5590418187382d570866a0a161268a407eb615
Benjamin Qi <bqi343@gmail.com>
2020-12-23 15:22:05 -0500

    add PDFs for exercise + sprinklers 2

--- solutions/1_extra_usaco_probs.ts
+++ solutions/1_extra_usaco_probs.ts
@@ -1,7 +1,9 @@
-import { Problem } from '../../../../content/models';
+import { Problem } from '../content/models';
+
+// add information for problems not associated with any module
 
 // take some stuff from https://github.com/bqi343/USACO/blob/2d1d50f10d0a489b98b64e60eca70442b6f843f4/Contests/USACO%20Links/Division-Specific/Platinum.md
-// add tags for problems not associated with any module
+// to be included in USACO division table
 
 export default [
   new Problem(
@@ -20,6 +22,7 @@ export default [
     'Normal',
     false,
     ['Prefix Sums', 'DP'],
-    ''
+    'usaco-1044'
   ),
+  new Problem('Plat', 'Exercise', '1045', 'Hard', false, null, 'usaco-1045'),
 ];
