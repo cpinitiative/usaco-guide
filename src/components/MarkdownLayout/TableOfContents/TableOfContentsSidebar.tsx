d0c07b22bf91e148db396add8014b4934ebfeedf
Benjamin Qi <bqi343@gmail.com>
2020-12-23 17:07:01 -0500

    start

--- src/components/MarkdownLayout/TableOfContents/TableOfContentsSidebar.tsx
+++ src/components/MarkdownLayout/TableOfContents/TableOfContentsSidebar.tsx
@@ -6,6 +6,7 @@ import { useMemo } from 'react';
 import genLinksFromTOCHeadings from './genLinksFromTOCHeadings';
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import getGithubUrl from './getGithubUrl';
+import LinksToEdit from './LinksToEdit';
 
 const TableOfContentsSidebar = ({
   tableOfContents,
@@ -32,27 +33,7 @@ const TableOfContentsSidebar = ({
       </h2>
       {links}
 
-      <OutboundLink
-        href={githubUrl}
-        target="_blank"
-        className="group block mt-8 transition duration-150 ease-in-out text-gray-600 hover:underline hover:text-blue-600 dark:text-dark-med-emphasis"
-      >
-        Edit on Github
-        <svg
-          className="w-4 h-5 mb-1 ml-1 inline-block text-gray-400 group-hover:text-blue-400"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-          />
-        </svg>
-      </OutboundLink>
+      <LinksToEdit className="group block mt-8 transition duration-150 ease-in-out text-gray-600 hover:underline hover:text-blue-600 dark:text-dark-med-emphasis" />
 
       {/* <OutboundLink
         href="https://www.reddit.com/r/usaco/"
--- src/components/MarkdownLayout/TableOfContents/getGithubUrl.tsx
+++ src/components/MarkdownLayout/TableOfContents/getGithubUrl.tsx
@@ -4,18 +4,35 @@ import { graphql, useStaticQuery } from 'gatsby';
 import { SolutionInfo } from '../../../models/solution';
 import { ModuleInfo } from '../../../models/module';
 
-export default function getGithubUrl() {
+export default function getInfoToEdit() {
   const moduleLayoutInfo = useContext(MarkdownLayoutContext);
   const markdownInfo = moduleLayoutInfo.markdownLayoutInfo;
   const relativePath = markdownInfo.fileRelativePath;
-  let githubUrl = 'https://github.com/cpinitiative/usaco-guide/blob/master/';
-
+  const githubUrlPrefix =
+    'https://github.com/cpinitiative/usaco-guide/blob/master/';
+  const rawGithubUrlPrefix =
+    'https://raw.githubusercontent.com/cpinitiative/usaco-guide/master/';
+  let suffix = '';
   if (markdownInfo instanceof ModuleInfo) {
     // /\d_.*/.test(relativePath)
-    githubUrl += 'content/' + relativePath;
+    suffix = 'content/' + relativePath;
   } else {
-    githubUrl += 'solutions/' + relativePath;
+    suffix = 'solutions/' + relativePath;
   }
-  githubUrl = githubUrl.replace(' ', '%20'); // is there some function that can automatically escape chars?
+
+  const escapeSpaces = url => url.replace(' ', '%20');
+  const githubUrl = escapeSpaces(githubUrlPrefix + suffix);
+  const rawGithubUrl = escapeSpaces(rawGithubUrlPrefix + suffix);
+  // is there some function that can automatically escape chars?
+
+  // https://www.scrapingbee.com/blog/web-scraping-javascript/
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  const getContents = async () => {
+    const data = await fetch(rawGithubUrl);
+    const text = await data.text();
+    console.log('TEXT', text); // ok what do I do with this
+  };
+
+  getContents();
   return githubUrl;
 }
