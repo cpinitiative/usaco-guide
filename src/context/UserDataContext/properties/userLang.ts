6fbd79de5206ebd6f36716ff443c7cd995c51608
Benjamin Qi <bqi343@gmail.com>
2021-01-01 23:12:30 -0500

    Update userLang.ts

--- src/context/UserDataContext/properties/userLang.ts
+++ src/context/UserDataContext/properties/userLang.ts
@@ -13,10 +13,45 @@ export type UserLangAPI = {
   setLang: (lang: Language) => void;
 };
 
+// From https://stackoverflow.com/questions/2090551/parse-query-string-in-javascript
+function getQueryVariable(query, variable): string {
+  const vars = query.split('&');
+  for (let i = 0; i < vars.length; i++) {
+    const pair = vars[i].split('=');
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return null;
+}
+
+const getLangFromUrl = () => {
+  return window.location?.search?.length > 0
+    ? getQueryVariable(window.location.search.slice(1), 'lang')
+    : null;
+};
+
 export default class UserLang extends SimpleUserDataPropertyAPI {
   protected storageKey = 'lang';
   protected defaultValue = 'cpp';
   protected setterFunctionName = 'setLang';
+
+  public initializeFromLocalStorage = () => {
+    this.value =
+      getLangFromUrl() ||
+      this.getValueFromLocalStorage(
+        this.getLocalStorageKey(this.storageKey),
+        this.defaultValue
+      );
+  };
+
+  public importValueFromObject = data => {
+    this.value =
+      getLangFromUrl() ||
+      (data.hasOwnProperty(this.storageKey) && data[this.storageKey] !== null
+        ? data[this.storageKey]
+        : this.defaultValue);
+  };
 
   // before page loads, we want to default to showing
   // all languages for SEO purposes
