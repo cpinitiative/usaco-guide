d8de4f5d010a1d3149c844e64c8dc7a2504ca63b
jeffreymeng <jeffkmeng@gmail.com>
2021-01-17 12:56:48 -0800

    feat: dont show homepage to nonregistered students

--- src/components/Classes/ClassLayout.tsx
+++ src/components/Classes/ClassLayout.tsx
@@ -3,7 +3,7 @@ import { Link, navigate } from 'gatsby';
 import * as Icons from 'heroicons-react';
 import DynamicMarkdownRenderer from '../DynamicMarkdownRenderer';
 import * as React from 'react';
-import { useContext, useState } from 'react';
+import { useContext, useMemo, useState } from 'react';
 import SEO from '../seo';
 import FirebaseContext from '../../context/FirebaseContext';
 import ClassContext from '../../context/ClassContext';
@@ -119,15 +119,12 @@ export default function ClassLayout({
     loading ||
     notFound ||
     error ||
-    showNotFound
-    // The below code would prevent non-members from viewing classes
-    // but it would also prevent non-member admins from viewing
-    // ||
-    // (!isInstructor &&
-    //   !(
-    //     userClasses.some((c: { id: string }) => c.id === classId) &&
-    //     userClassIds.includes(classId)
-    //   ))
+    showNotFound ||
+    (!isInstructor &&
+      !(
+        userClasses.some((c: { id: string }) => c.id === classId) &&
+        userClassIds.includes(classId)
+      ))
   ) {
     return (
       <>
