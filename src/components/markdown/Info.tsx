8a1780f656dd7ba66343f66364b1e8b4dfdffbce
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-01-28 17:53:24 -0600

    updated icon

--- src/components/markdown/Info.tsx
+++ src/components/markdown/Info.tsx
@@ -12,14 +12,11 @@ const Info = ({
       <div className="flex-shrink-0">
         <svg
           className="h-5 w-5 text-blue-400"
+          viewBox="0 0 24 24"
           fill="currentColor"
-          viewBox="0 0 20 20"
         >
-          <path
-            fillRule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-            clipRule="evenodd"
-          />
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
         </svg>
       </div>
       <div className="ml-3 flex-1">
