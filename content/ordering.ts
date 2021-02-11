1da2220416657aa9bdcf4f8a668e736ac8337f90
HarryWangATX <harrynicomama@gmail.com>
2021-02-11 12:04:07 -0600

    refactored code

--- content/ordering.ts
+++ content/ordering.ts
@@ -94,7 +94,10 @@ const MODULE_ORDERING: { [key in SectionID]: Chapter[] } = {
   silver: [
     {
       name: 'Prefix Sums',
-      items: ['prefix-sums', 'prefix-sums-2'],
+      items: [
+        'prefix-sums', 
+        'prefix-sums-2',
+      ],
     },
     {
       name: 'Sorting & Searching',
