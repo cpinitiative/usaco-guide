72c93c0db8fe8fecdbfd98766698417851cf6fdf
Benjamin Qi <bqi343@gmail.com>
2021-02-16 18:51:07 -0500

    link to module in sols, also update format for some sols + in contributing

--- src/context/MarkdownLayoutContext.tsx
+++ src/context/MarkdownLayoutContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import * as React from 'react';
+// import * as React from 'react';
 import { ModuleInfo, ModuleLinkInfo, ModuleProgress } from '../models/module';
 import { SolutionInfo } from '../models/solution';
 
@@ -7,6 +7,7 @@ const MarkdownLayoutContext = createContext<{
   markdownLayoutInfo: ModuleInfo | SolutionInfo;
   sidebarLinks: ModuleLinkInfo[];
   activeIDs: string[];
+  problemURLs: string[];
   isMobileNavOpen: boolean;
   setIsMobileNavOpen: (x: boolean) => void;
   moduleProgress: ModuleProgress;
