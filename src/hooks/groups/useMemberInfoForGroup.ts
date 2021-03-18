9576285822acaf1380887923d4d31e78d3e17e2f
Nathan Wang <nathan.r.wang@gmail.com>
2021-03-18 00:04:09 -0700

    feat: cache member info

--- src/hooks/groups/useMemberInfoForGroup.ts
+++ src/hooks/groups/useMemberInfoForGroup.ts
@@ -4,6 +4,8 @@ import firebaseType from 'firebase';
 import useFirebase from '../useFirebase';
 import { useNotificationSystem } from '../../context/NotificationSystemContext';
 
+let cachedData: firebaseType.UserInfo[] = null;
+
 export default function getMemberInfoForGroup(group: GroupData) {
   const [memberInfo, setMemberInfo] = React.useState<firebaseType.UserInfo[]>(
     null
@@ -21,28 +23,36 @@ export default function getMemberInfoForGroup(group: GroupData) {
         ...group.adminIds,
       ];
 
-      firebase
-        .functions()
-        .httpsCallable('getUsers')({
-          users: userIDs.map(id => ({
-            uid: id,
-          })),
-        })
-        .then(d => {
-          if (d?.data?.users?.length > 0) {
-            setMemberInfo(d.data.users);
-          } else {
-            notifications.addNotification({
-              level: 'error',
-              message: 'Error: Failed to fetch member info for leaderboard',
-            });
-          }
-        })
-        .catch(e => {
-          notifications.showErrorNotification(e);
-        });
+      if (
+        cachedData &&
+        userIDs.every(id => cachedData.some(person => person.uid === id))
+      ) {
+        setMemberInfo(cachedData);
+      } else {
+        firebase
+          .functions()
+          .httpsCallable('getUsers')({
+            users: userIDs.map(id => ({
+              uid: id,
+            })),
+          })
+          .then(d => {
+            if (d?.data?.users?.length > 0) {
+              setMemberInfo(d.data.users);
+              cachedData = d.data.users;
+            } else {
+              notifications.addNotification({
+                level: 'error',
+                message: 'Error: Failed to fetch member info for leaderboard',
+              });
+            }
+          })
+          .catch(e => {
+            notifications.showErrorNotification(e);
+          });
+      }
     },
-    [group]
+    [group.memberIds, group.ownerIds, group.adminIds]
   );
 
   return memberInfo;
