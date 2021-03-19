4248e1241ac3c46717e1ddc1e213f14c0edae31a
Nathan Wang <nathan.r.wang@gmail.com>
2021-03-18 18:47:00 -0700

    update types for member info

--- src/hooks/groups/useMemberInfoForGroup.ts
+++ src/hooks/groups/useMemberInfoForGroup.ts
@@ -4,15 +4,19 @@ import firebaseType from 'firebase';
 import useFirebase from '../useFirebase';
 import { useNotificationSystem } from '../../context/NotificationSystemContext';
 
+export type MemberInfo = {
+  displayName: string;
+  uid: string;
+  photoURL: string;
+};
+
 let cachedData: {
   groupId: string;
-  data: firebaseType.UserInfo[];
+  data: MemberInfo[];
 } = null;
 
 export default function getMemberInfoForGroup(group: GroupData) {
-  const [memberInfo, setMemberInfo] = React.useState<firebaseType.UserInfo[]>(
-    null
-  );
+  const [memberInfo, setMemberInfo] = React.useState<MemberInfo[]>(null);
   const notifications = useNotificationSystem();
 
   useFirebase(
