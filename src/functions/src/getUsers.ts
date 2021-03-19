c8bd44caf4d890ca52eeee9ddc7f79750393bee6
Jeffrey Meng <jeffkmeng@gmail.com>
2021-03-18 21:53:32 -0700

    feat: upgrade getUsers

--- src/functions/src/getUsers.ts
+++ src/functions/src/getUsers.ts
@@ -6,7 +6,7 @@ if (admin.apps.length === 0) {
 }
 
 export default functions.https.onCall(
-  (
+  async (
     data: {
       users: ({ uid: string } | { email: string })[];
     },
@@ -14,6 +14,7 @@ export default functions.https.onCall(
   ) => {
     const { users } = data;
     const callerUid = context.auth?.uid;
+    const caller = await admin.auth().getUser(callerUid);
 
     if (!callerUid || !users || users.length === 0) {
       throw new functions.https.HttpsError(
@@ -21,24 +22,60 @@ export default functions.https.onCall(
         'The function was not called with the correct data, or the caller is not logged in.'
       );
     }
-    return admin
+    const classesPermissions = await admin
       .firestore()
       .collection('classes')
       .doc('permissions')
       .get()
-      .then(snapshot => snapshot.data())
-      .then(permissions => {
-        if (
-          !permissions?.admins?.includes(callerUid) &&
-          !permissions?.instructors?.includes(callerUid)
-        ) {
-          throw new functions.https.HttpsError(
-            'permission-denied',
-            'Caller is not an admin or instructor.'
-          );
-        } else {
-          return admin.auth().getUsers(users);
-        }
-      });
+      .then(snapshot => snapshot.data());
+    if (
+      !caller.customClaims.isAdmin &&
+      !classesPermissions?.admins?.includes(callerUid) &&
+      !classesPermissions?.instructors?.includes(callerUid)
+    ) {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        'Caller is not an admin or instructor.'
+      );
+    }
+    const getUserPromises: Promise<admin.auth.GetUsersResult>[] = [];
+    while (users.length > 0) {
+      getUserPromises.push(admin.auth().getUsers(users.splice(0, 100)));
+    }
+    return (await Promise.all(getUserPromises)).reduce(
+      (acc, el) => {
+        return {
+          notFound: [...acc.notFound, ...el.notFound],
+          users: [
+            ...acc.users,
+            ...el.users.map(
+              ({
+                customClaims,
+                uid,
+                email,
+                displayName,
+                photoURL,
+                disabled,
+                metadata,
+                providerData,
+              }) => ({
+                customClaims,
+                uid,
+                email,
+                displayName,
+                photoURL,
+                disabled,
+                metadata,
+                providerData,
+              })
+            ),
+          ],
+        };
+      },
+      {
+        notFound: [],
+        users: [],
+      }
+    );
   }
 );
