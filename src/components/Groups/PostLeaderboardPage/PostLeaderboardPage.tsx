6820888227a5679471c361af542a9867cd83b01d
Nathan Wang <nathan.r.wang@gmail.com>
2021-03-18 17:53:17 -0700

    update leaderboard table shadow

--- src/components/Groups/PostLeaderboardPage/PostLeaderboardPage.tsx
+++ src/components/Groups/PostLeaderboardPage/PostLeaderboardPage.tsx
@@ -81,7 +81,7 @@ export default function PostLeaderboardPage(props) {
         <div className="flex flex-col">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="overflow-hidden border-b border-gray-200 dark:border-transparent sm:rounded-lg">
+              <div className="overflow-hidden shadow border-b border-gray-200 dark:border-transparent sm:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                   <thead className="bg-gray-50 dark:bg-gray-800">
                     <tr>
