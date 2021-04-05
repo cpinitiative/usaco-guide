9c49145e0ad2e090078d96ca134f4ca4dce29455
MrinallU <61529929+MrinallU@users.noreply.github.com>
2021-04-04 22:18:51 -0400

    Update src/components/markdown/CodeBlock/CodeBlock.tsx
    Co-authored-by: Jeffrey Meng <jeffreymeng@users.noreply.github.com>

--- src/components/markdown/CodeBlock/CodeBlock.tsx
+++ src/components/markdown/CodeBlock/CodeBlock.tsx
@@ -297,7 +297,7 @@ class CodeBlock extends React.Component<
                 ? this.renderTokens(tokens, 10, getLineProps, getTokenProps)
                 : this.renderTokens(tokens, -1, getLineProps, getTokenProps)}
               {tokens.length > 15 && !collapsed && <div className="h-8" />}
-              {this.props.isCodeBlockExpandable && collapsed && tokens.length > 15 && (
+              {isCodeBlockExpandable && collapsed && tokens.length > 15 && (
                 <div
                   className={
                     (collapsed ? 'h-full' : 'h-12') +
