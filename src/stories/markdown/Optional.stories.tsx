bf8d7f7fc17ad4b9732b7cb57681c2775e3b9462
Nathan Wang <nathan.r.wang@gmail.com>
2021-04-08 21:01:25 -0700

    update optional block story

--- src/stories/markdown/Optional.stories.tsx
+++ src/stories/markdown/Optional.stories.tsx
@@ -21,36 +21,36 @@ Default.args = {
   children: (
     <>
       <MiscMarkdownContent />
-      <HTMLComponents.p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atque
-        consectetur delectus dolorem fuga illum libero molestias mollitia nam
-        neque, nostrum numquam possimus provident reprehenderit sapiente vel
-        vero voluptatem voluptatibus.
-      </HTMLComponents.p>
-      <HTMLComponents.p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atque
-        consectetur delectus dolorem fuga illum libero molestias mollitia nam
-        neque, nostrum numquam possimus provident reprehenderit sapiente vel
-        vero voluptatem voluptatibus.
-      </HTMLComponents.p>
-      <HTMLComponents.p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atque
-        consectetur delectus dolorem fuga illum libero molestias mollitia nam
-        neque, nostrum numquam possimus provident reprehenderit sapiente vel
-        vero voluptatem voluptatibus.
-      </HTMLComponents.p>
-      <HTMLComponents.p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atque
-        consectetur delectus dolorem fuga illum libero molestias mollitia nam
-        neque, nostrum numquam possimus provident reprehenderit sapiente vel
-        vero voluptatem voluptatibus.
-      </HTMLComponents.p>
-      <HTMLComponents.p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atque
-        consectetur delectus dolorem fuga illum libero molestias mollitia nam
-        neque, nostrum numquam possimus provident reprehenderit sapiente vel
-        vero voluptatem voluptatibus.
-      </HTMLComponents.p>
+      <p className="mb-4">
+        <a
+          target="_blank"
+          href="https://arxiv.org/pdf/1904.01210.pdf"
+          rel="nofollow noopener noreferrer"
+        >
+          Paper
+        </a>
+      </p>
+      <blockquote>
+        <HTMLComponents.p>
+          A common mistake in implementing the Floyd–Warshall algorithm is to
+          misorder the triply nested loops (The correct order is{' '}
+          <HTMLComponents.inlineCode>KIJ</HTMLComponents.inlineCode>). The
+          incorrect <HTMLComponents.inlineCode>IJK</HTMLComponents.inlineCode>{' '}
+          and
+          <HTMLComponents.inlineCode>IKJ</HTMLComponents.inlineCode> algorithms
+          do not give correct solutions for some instance. However, we can prove
+          that if these are repeated three times, we obtain the correct
+          solutions.
+        </HTMLComponents.p>
+        <HTMLComponents.p>
+          It would be emphasized that these fixes (repeating incorrect
+          algorithms three times) have the same time complexity as the correct
+          Floyd–Warshall algorithm up to constant factors. Therefore, our
+          results suggest that, if one is confused by the order of the triply
+          nested loops, one can repeat the procedure three times just to be
+          safe.
+        </HTMLComponents.p>
+      </blockquote>
     </>
   ),
 };
