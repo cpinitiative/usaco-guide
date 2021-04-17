f0bd7bd5125c5ab3f2a1731fc92f3c36947fe595
Nathan Wang <nathan.r.wang@gmail.com>
2021-04-17 16:23:23 -0700

    ci: only run lhci on some paths

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,14 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: './public',
+      url: [
+        'http://localhost/',
+        'http://localhost/dashboard',
+        'https://usaco.guide/general/',
+        'https://usaco.guide/general/using-this-guide',
+      ],
+    },
     upload: {
       target: 'temporary-public-storage',
     },
