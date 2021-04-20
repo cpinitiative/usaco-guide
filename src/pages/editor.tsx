8198547d85e89dbb02492de10c82a56585c6b151
Nathan Wang <nathan.r.wang@gmail.com>
2021-04-19 22:23:51 -0700

    add tab bar to editor

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -19,6 +19,7 @@ import Split from 'react-split';
 import styled from 'styled-components';
 import problemsSchema from '../../content/problems.schema.json';
 import ButtonGroup from '../components/ButtonGroup';
+import EditorTabBar from '../components/Editor/EditorTabBar';
 import {
   conf as mdxConf,
   language as mdxLang,
@@ -284,6 +285,14 @@ export default function LiveUpdatePage(props: PageProps) {
                 className="h-full tw-forms-disable"
                 style={{ minWidth: '300px' }}
               >
+                <EditorTabBar
+                  tabs={[
+                    { label: 'Contributing.mdx' },
+                    { label: 'Prefix_Sums.mdx' },
+                  ]}
+                  activeTab={'Contributing.mdx'}
+                  onTabSelect={tab => console.log(tab)}
+                />
                 <Editor
                   theme="vs-dark"
                   path={
