1fe576515bc41366e44ab6d0dc6ef0bfe1674b31
maggie-j-liu <maggie.j.liu@gmail.com>
2021-06-18 16:54:23 -0700

    issue -> pull request

--- src/components/ProblemSuggestionModal.tsx
+++ src/components/ProblemSuggestionModal.tsx
@@ -435,12 +435,12 @@ export default function ProblemSuggestionModal({
                 <br />
                 This will be submitted as a public{' '}
                 <a
-                  href="https://github.com/cpinitiative/usaco-guide/issues"
+                  href="https://github.com/cpinitiative/usaco-guide/pulls"
                   target="_blank"
                   rel="noreferrer"
                   className="text-blue-600 dark:text-blue-300 underline"
                 >
-                  Github issue
+                  Github pull request
                 </a>
                 .
               </p>
