9b7d9f33d81a9c619b4221a290f6ea880d57c717
Nathan Wang <nathan.r.wang@gmail.com>
2021-07-08 16:21:39 -0700

    roll back storybook to webpack 4

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
