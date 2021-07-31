c02438c66704efbe34152cd825fdde0987b2cb3b
Ryan Chou <81596991+ryanchou-dev@users.noreply.github.com>
2021-07-31 11:02:40 -0700

    class to className

--- src/components/Index/ContributorsSection.tsx
+++ src/components/Index/ContributorsSection.tsx
@@ -29,7 +29,7 @@ export default function ContributorsSection() {
               </a>{' '}
               If you're in the United States, you can earn volunteer hours too!
             </p>
-            <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10">
+            <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10">
               Current Content Authors:
             </h1>
             <ul className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
@@ -60,7 +60,7 @@ export default function ContributorsSection() {
                 </li>
               ))}
             </ul>
-            <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10">
+            <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10">
               Past Content Authors:
             </h1>
             <ul className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
