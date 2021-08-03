6b8c4bd61c660726def8e03bd0e482702683a0b9
Ryan Chou <81596991+ryanchou-dev@users.noreply.github.com>
2021-08-03 14:39:50 -0700

    remove since data for past content authors

--- src/components/Index/ContributorsSection.tsx
+++ src/components/Index/ContributorsSection.tsx
@@ -119,9 +119,6 @@ export default function ContributorsSection(): JSX.Element {
                           <h3>{author.name}</h3>
                         </a>{' '}
                       </div>
-                      <p className="text-gray-500 dark:text-gray-400 list-disc list-inside text-xs space-y-2 mb-6">
-                        Since {author.sinceDate}
-                      </p>
                     </div>
                   </div>
                 </li>
