cf33469d5ca6241f43ef63c433e2cf647f4975dc
Nathan Gong <nathan.gong22@bcp.org>
2021-08-02 17:29:46 -0700

    Reduce y padding

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -175,7 +175,7 @@ export default function TopNavigationBar({
                                 indexPage
                                   ? 'max-w-6xl px-2 lg:px-6'
                                   : 'max-w-7xl px-2 sm:px-4 lg:px-8',
-                                'mx-auto grid gap-y-6 py-6 sm:grid-cols-2 sm:gap-8 sm:px-6 sm:py-8 lg:grid-cols-4 lg:px-8 lg:py-12 xl:py-16'
+                                'mx-auto grid gap-y-6 py-6 sm:grid-cols-2 sm:gap-8 sm:px-6 sm:py-8 lg:grid-cols-4 lg:px-8 lg:py-12'
                               )}
                             >
                               {userGroups.data?.length > 0 && (
