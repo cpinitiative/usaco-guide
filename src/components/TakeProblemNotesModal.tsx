311e6bdddbe77dd50584ec3dbdb3cd346b139773
Mrinall <bossguy124567@gmail.com>
2021-08-18 20:07:26 -0400

    Edit notes modal

--- src/components/TakeProblemNotesModal.tsx
+++ src/components/TakeProblemNotesModal.tsx
@@ -1,13 +1,15 @@
-import { Transition } from '@headlessui/react';
+import { Dialog, Transition } from '@headlessui/react';
+import { XIcon } from '@heroicons/react/solid';
 import { EditorState } from 'draft-js';
 import * as React from 'react';
+import { Fragment } from 'react';
 
 export default function TakeProblemNotesModal({
   isOpen,
   onClose,
 }: {
   isOpen: boolean;
-  onClose: (editorState: any) => void;
+  onClose: any;
 }) {
   const [editorState, setEditorState] = React.useState(() =>
     EditorState.createEmpty()
@@ -21,36 +23,106 @@ export default function TakeProblemNotesModal({
   }, [isOpen]);
 
   return (
-    <Transition
-      show={isOpen}
-      className="fixed z-30 inset-0 h-full overflow-y-auto"
-    >
-      <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
-        <button
-          type="button"
-          onClick={() => onClose(editorState)} // pass in the current problem note state
-          className="bg-white dark:bg-dark-surface rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none"
-        >
-          <span className="sr-only">Close</span>
-          {/* Heroicon name: x */}
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+    // <Transition
+    //   show={isOpen}
+    //   className="fixed z-30 inset-0 h-full overflow-y-auto"
+    // >
+    //   <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+    //     <button
+    //       type="button"
+    //       onClick={() => onClose(editorState)} // pass in the current problem note state
+    //       className="bg-white dark:bg-dark-surface rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none"
+    //     >
+    //       <span className="sr-only">Close</span>
+    //       {/* Heroicon name: x */}
+    //       <svg
+    //         className="h-6 w-6"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         fill="none"
+    //         viewBox="0 0 24 24"
+    //         stroke="currentColor"
+    //         aria-hidden="true"
+    //       >
+    //         <path
+    //           strokeLinecap="round"
+    //           strokeLinejoin="round"
+    //           strokeWidth="2"
+    //           d="M6 18L18 6M6 6l12 12"
+    //         />
+    //       </svg>
+    //     </button>
+    //   </div>
+    //   Take some notes!
+    // </Transition>
+
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        static
+        className="fixed z-10 inset-0 overflow-y-auto"
+        open={isOpen}
+        onClose={() => onClose()}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 dark:bg-gray-900 dark:bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
             aria-hidden="true"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      </div>
-      Take some notes!
-    </Transition>
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom bg-white dark:bg-dark-surface rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+              <div>
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-200"
+                >
+                  Sign In
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
+                    Signing in allows you to sync your data with our servers
+                    (highly recommended). If this is your first time signing in,
+                    you will have the option to upload your local data to the
+                    server.
+                  </p>
+                </div>
+              </div>
+              <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                <button
+                  type="button"
+                  className="bg-white dark:bg-dark-surface rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-dark-surface focus:ring-blue-500"
+                  onClick={() => onClose()}
+                >
+                  <span className="sr-only">Close</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
