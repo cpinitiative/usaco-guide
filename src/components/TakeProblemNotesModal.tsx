a3fbe763ad44fe71742d08cbaab23d1a865e6fda
Mrinall <bossguy124567@gmail.com>
2021-08-18 20:42:00 -0400

    Create Draft.js instance

--- src/components/TakeProblemNotesModal.tsx
+++ src/components/TakeProblemNotesModal.tsx
@@ -15,6 +15,10 @@ export default function TakeProblemNotesModal({
     EditorState.createEmpty()
   );
   const [loading, setLoading] = React.useState(false);
+  const editor = React.useRef(null);
+  function focusEditor() {
+    editor.current.focus();
+  }
 
   React.useEffect(() => {
     if (isOpen) {
@@ -105,8 +109,16 @@ export default function TakeProblemNotesModal({
                 {/*     Description*/}
                 {/*  </p>*/}
                 {/*</div>*/}
-                <div>
+                <div
+                  style={{
+                    border: '1px solid black',
+                    minHeight: '6em',
+                    cursor: 'text',
+                  }}
+                  onClick={focusEditor}
+                >
                   <Editor
+                    ref={editor}
                     editorState={editorState}
                     onChange={setEditorState}
                     placeholder="Write something!"
