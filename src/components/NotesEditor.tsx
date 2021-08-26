52fc7d8e1c1a68dea0a7fd8eafeee759e5358c68
Mrinall <bossguy124567@gmail.com>
2021-08-26 19:22:41 -0400

    Fix DOM error

--- src/components/NotesEditor.tsx
+++ src/components/NotesEditor.tsx
@@ -1,62 +1,48 @@
 //https://codepen.io/FLCcrakers/pen/JZVeZE?editors=0111
 
-import Loadable from '@loadable/component';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ReactQuill from 'react-quill';
 
-type MyState = { text: string };
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link', 'image'],
+  ],
+};
 
-class NotesEditor extends Component<any, MyState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "<div contenteditable='false'>wo</div>",
-    };
+const formats = [
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+];
+
+function NotesEditor() {
+  useEffect(() => {
+    if (typeof window === 'undefined' || !window.document) {
+      console.log(
+        `bailing out of the useeffect. Going to continue to render??`
+      );
+      return;
+    }
+  }, []);
+
+  console.log(`reached before render`);
+
+  const isBrowser = typeof window !== 'undefined';
+  if (!isBrowser) {
+    return;
   }
-
-  modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      ['link', 'image'],
-    ],
-  };
-
-  formats = [
-    'header',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-  ];
-  handleProcedureContentChange = (content, delta, source, editor) => {
-    //let has_attribues = delta.ops[1].attributes || "";
-    //console.log(has_attribues);
-    //const cursorPosition = e.quill.getSelection().index;
-    // this.quill.insertText(cursorPosition, "★");
-    //this.quill.setSelection(cursorPosition + 1);
-  };
-
-  render() {
-    return (
-      <ReactQuill
-        theme="snow"
-        modules={this.modules}
-        formats={this.formats}
-        value={this.state.text}
-        onChange={this.handleProcedureContentChange}
-      >
-        <div className="my-editing-area" />
-      </ReactQuill>
-    );
-  }
+  return <ReactQuill theme="snow" formats={formats} modules={modules} />;
 }
-const Editor = Loadable(() => import('./NotesEditor'));
-export default Editor;
+
+export default NotesEditor;
