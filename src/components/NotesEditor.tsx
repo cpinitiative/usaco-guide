e35607fd16e69cd95a61b67c8c44451d94d1e1ee
Mrinall <bossguy124567@gmail.com>
2021-08-27 21:50:58 -0400

    Fix quill

--- src/components/NotesEditor.tsx
+++ src/components/NotesEditor.tsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
+let ReactQuill;
 export default class FormHtmlEditor extends Component {
-  private ReactQuill = false;
-  constructor(props) {
-    super(props);
-    if (typeof window !== 'undefined') {
-      this.ReactQuill = require('react-quill');
-    }
+  componentDidMount() {
+    require.ensure([], function (require) {
+      ({ ReactQuill } = require('react-quill'));
+    });
   }
 
   render() {
-    const ReactQuill = this.ReactQuill;
     if (typeof window !== 'undefined' && ReactQuill) {
       return <ReactQuill theme="snow" value="Hello" />;
     } else {
