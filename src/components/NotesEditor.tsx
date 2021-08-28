0e97087e5e37cf2e55f80c1f113fc42db37d9742
Mrinall <bossguy124567@gmail.com>
2021-08-27 22:06:02 -0400

    Add toolbar

--- src/components/NotesEditor.tsx
+++ src/components/NotesEditor.tsx
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 
 let ReactQuill;
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    ['link', 'code'],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'code',
+];
+
 export default class FormHtmlEditor extends Component {
   componentDidMount() {
     require.ensure([], function (require) {
@@ -10,8 +39,17 @@ export default class FormHtmlEditor extends Component {
 
   render() {
     if (typeof window !== 'undefined' && ReactQuill) {
-      return <ReactQuill theme="snow" value="Hello" />;
+      console.log('Quill Active');
+      return (
+        <ReactQuill
+          theme="snow"
+          value="Hello"
+          modules={modules}
+          formats={formats}
+        />
+      );
     } else {
+      console.log('Quill inactive');
       return <textarea />;
     }
   }
