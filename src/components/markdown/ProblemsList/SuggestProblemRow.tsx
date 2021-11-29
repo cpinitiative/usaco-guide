d3b79a5ff1e2c5b24c34a4eb89fcc1ef179e5ef6
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-11-28 23:55:12 -0600

    fix suggestions

--- src/components/markdown/ProblemsList/SuggestProblemRow.tsx
+++ src/components/markdown/ProblemsList/SuggestProblemRow.tsx
@@ -6,7 +6,7 @@ import { EditorContext } from '../../../context/EditorContext';
 import ProblemSuggestionModalContext from '../../../context/ProblemSuggestionModalContext';
 
 export default function SuggestProblemRow({ listName }: { listName: string }) {
-  const context = useContext(ProblemSuggestionModalContext);
+  // const context = useContext(ProblemSuggestionModalContext);
   const { inEditor } = useContext(EditorContext);
   const path = globalHistory.location.pathname || '';
   return (
@@ -17,7 +17,7 @@ export default function SuggestProblemRow({ listName }: { listName: string }) {
         >
           <button
             className="w-full text-center text-sm uppercase font-medium py-2 text-gray-600 hover:text-blue-700 dark:text-gray-300 dark:hover:text-blue-200 hover:underline"
-            onClick={() => context.openProblemSuggestionModal(listName)}
+            // onClick={() => context.openProblemSuggestionModal(listName)}
           >
             + {inEditor ? 'Add a Problem' : 'Suggest a Problem'}
           </button>
