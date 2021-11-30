123d41351d6d928b96209b3440aa76c38b45c090
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-11-29 19:41:13 -0600

    Update SuggestProblemRow.tsx

--- src/components/markdown/ProblemsList/SuggestProblemRow.tsx
+++ src/components/markdown/ProblemsList/SuggestProblemRow.tsx
@@ -12,8 +12,9 @@ export default function SuggestProblemRow({ listName }: { listName: string }) {
   return (
     <tr>
       <td colSpan={10}>
-        <Link
-          to={`https://docs.google.com/forms/d/e/1FAIpQLScSnHRAV1ybiwHMBH-byqhAijRlpNKLnxzpMLYUK8mUXyMyEg/viewform?usp=pp_url&entry.1983837292=${path}`}
+        <a
+          href={`https://docs.google.com/forms/d/e/1FAIpQLScSnHRAV1ybiwHMBH-byqhAijRlpNKLnxzpMLYUK8mUXyMyEg/viewform?usp=pp_url&entry.1983837292=${path}`}
+          target="_blank"
         >
           <button
             className="w-full text-center text-sm uppercase font-medium py-2 text-gray-600 hover:text-blue-700 dark:text-gray-300 dark:hover:text-blue-200 hover:underline"
@@ -21,7 +22,7 @@ export default function SuggestProblemRow({ listName }: { listName: string }) {
           >
             + {inEditor ? 'Add a Problem' : 'Suggest a Problem'}
           </button>
-        </Link>
+        </a>
       </td>
     </tr>
   );
