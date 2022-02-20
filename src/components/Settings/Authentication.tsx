a32943bfb44906bca77f7e0a701ddc711271c66b
Nathan Gong <nathangong1234@gmail.com>
2022-02-19 17:33:18 -0800

    Remove link option

--- src/components/Settings/Authentication.tsx
+++ src/components/Settings/Authentication.tsx
@@ -119,25 +119,6 @@ export default function Authentication() {
               </div>
             </div>
           </fieldset>
-
-          <div className="h-4" />
-
-          {!linkedWithGoogle && (
-            <button
-              className="inline-flex items-center px-4 py-2 border border-gray-300 dark:border-gray-600 text-sm leading-5 font-medium rounded-md text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
-              onClick={null}
-            >
-              Link Account with Google
-            </button>
-          )}
-          {!linkedWithGithub && (
-            <button
-              className="inline-flex items-center px-4 py-2 border border-gray-300 dark:border-gray-600 text-sm leading-5 font-medium rounded-md text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
-              onClick={null}
-            >
-              Link Account with Github
-            </button>
-          )}
         </div>
       ) : (
         <p>You need to be logged in to update your profile.</p>
