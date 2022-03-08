0b154143ad36e095071ab8714d4953f2ab2fc03b
nguyenvn2905 <88864939+nguyenvn2905@users.noreply.github.com>
2022-03-08 16:19:50 +0700

    Create template.js

---
+++ template.js
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<math.h>
+#include<numeric>
+#include<iomanip>
+#define FOR(x,a,b) for (int x=a;x<=b;x++)
+#define FOD(x,a,b) for (int x=a;x>=b;x--)
+#define pub push_back
+#define pob pop_back
+#define ii pair<int,int>
+#define pll pair<long long, long long>
+#define F first
+#define S second
+typedef unsigned long long int ull;
+typedef long long ll;
+const long N=1e5;
+using namespace std;
+void solve(){
+
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//  freopen("","r",stdin);
+//  freopen("","w",stdout);
+    solve();  
+    return 0;
+}
