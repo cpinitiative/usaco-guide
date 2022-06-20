e4f87d552c248cc81bcc1a07a1aa4a2f435f5e53
Maggie Liu <63619830+maggie-j-liu@users.noreply.github.com>
2022-06-20 19:19:40 +0000

    correct colors on first load

--- src/components/Index/sponsor-logos/EasyFunCoding.tsx
+++ src/components/Index/sponsor-logos/EasyFunCoding.tsx
@@ -1,25 +1,25 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
-import { useDarkMode } from '../../../context/DarkModeContext';
 const EasyFunCoding = () => {
-  const dark = useDarkMode();
-  if (dark) {
-    return (
-      <StaticImage
-        src="../../../assets/easyfuncoding.png"
-        alt="EasyFunCoding logo"
-        placeholder="blurred"
-        height={48}
-      />
-    );
-  }
   return (
-    <StaticImage
-      src="../../../assets/easyfuncoding.jpg"
-      alt="EasyFunCoding logo"
-      placeholder="blurred"
-      height={48}
-    />
+    <>
+      <div className="hidden dark:block">
+        <StaticImage
+          src="../../../assets/easyfuncoding.png"
+          alt="EasyFunCoding logo"
+          placeholder="blurred"
+          height={48}
+        />
+      </div>
+      <div className="dark:hidden">
+        <StaticImage
+          src="../../../assets/easyfuncoding.jpg"
+          alt="EasyFunCoding logo"
+          placeholder="blurred"
+          height={48}
+        />
+      </div>
+    </>
   );
 };
 export default EasyFunCoding;
