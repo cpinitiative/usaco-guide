8bb60f61875206f58a24cd49224dedba75d26174
Benjamin Qi <bqi343@gmail.com>
2023-02-05 09:32:43 -0500

    fix more imports

--- src/models/problem.ts
+++ src/models/problem.ts
@@ -1,7 +1,7 @@
 import { slug } from 'github-slugger';
 import * as defaultOrdering from '../../content/ordering';
 import PGS from '../components/markdown/PGS';
-import id_to_sol from '../components/markdown/ProblemsList/DivisionList/id_to_sol';
+import id_to_sol from '../components/markdown/ProblemsList/DivisionList/id_to_sol.json';
 import { books } from '../utils/books';
 export const contests = {
   CCC: ['DMOJ', 'Canadian Computing Competition'],
