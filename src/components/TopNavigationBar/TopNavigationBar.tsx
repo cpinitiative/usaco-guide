7c7148d36abc10f35628bcfce99ea3c0b9a72ecd
mjh316 <justinhuang316@gmail.com>
2023-03-19 21:04:07 -0700

    CPI Classes -> USACO Classes on banner

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -155,7 +155,9 @@ export default function TopNavigationBar({
           <div className="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
             <div className="pr-16 sm:text-center sm:px-16">
               <p className="font-medium text-white">
-                <span className="md:inline">Sign up for CPI Classes now!</span>
+                <span className="md:inline">
+                  Sign up for USACO Classes now!
+                </span>
                 <span className="block sm:ml-2 sm:inline-block">
                   <a
                     href="https://joincpi.org/classes"
