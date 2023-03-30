9bd7eb4f59037c11a1a394d560561014eba66484
Benjamin Qi <bqi343@gmail.com>
2023-03-29 22:13:34 -0400

    Update UserDataContext.tsx

--- src/context/UserDataContext/UserDataContext.tsx
+++ src/context/UserDataContext/UserDataContext.tsx
@@ -276,6 +276,9 @@ export const UserDataProvider = ({
     UserDataContextAPIs.forEach(api => api.initializeFromLocalStorage());
   }, []);
 
+  const [CREATING_ACCOUNT_FOR_FIRST_TIME, setCREATING_ACCOUNT_FOR_FIRST_TIME] =
+    useState(undefined);
+
   // If the user is signed in, sync remote data with local data
   React.useEffect(() => {
     if (firebaseUser) {
@@ -312,6 +315,10 @@ export const UserDataProvider = ({
           data = data || {};
           UserDataContextAPIs.forEach(api => api.importValueFromObject(data));
           UserDataContextAPIs.forEach(api => api.writeValueToLocalStorage());
+
+          setCREATING_ACCOUNT_FOR_FIRST_TIME(
+            data['CREATING_ACCOUNT_FOR_FIRST_TIME']
+          );
           setIsLoaded(true);
           triggerRerender();
         },
@@ -359,6 +366,9 @@ export const UserDataProvider = ({
           'Import user data (beta)? All existing data will be lost. Make sure to back up your data before proceeding.'
         )
       ) {
+        if (CREATING_ACCOUNT_FOR_FIRST_TIME !== undefined)
+          data['CREATING_ACCOUNT_FOR_FIRST_TIME'] =
+            CREATING_ACCOUNT_FOR_FIRST_TIME;
         UserDataContextAPIs.forEach(api => api.importValueFromObject(data));
         UserDataContextAPIs.forEach(api => api.writeValueToLocalStorage());
         if (firebaseUser) {
