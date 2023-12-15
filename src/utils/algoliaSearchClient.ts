08bd022e1657d2260cfd613098a87a4949056414
devo1ution <danielfangzhu@gmail.com>
2023-12-15 08:09:46 -0800

    somehow it works now

--- src/utils/algoliaSearchClient.ts
+++ src/utils/algoliaSearchClient.ts
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 
 export const searchClient = algoliasearch(
-  '3CFULMFIDW',
-  'b1b046e97b39abe6c905e0ad1df08d9e'
+  process.env.ALGOLIA_APP_ID ?? '3CFULMFIDW',
+  process.env.ALGOLIA_API_KEY ?? 'b1b046e97b39abe6c905e0ad1df08d9e'
 );
