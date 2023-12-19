b638d1751e0d384083e7a0b68731d1818ef40017
devo1ution <danielfangzhu@gmail.com>
2023-12-18 21:09:32 -0800

    working scraper!

--- usaco_util.mjs
+++ usaco_util.mjs
@@ -1,3 +1,4 @@
+// run `node usaco_util.mjs`
 import { readFileSync, writeFileSync } from 'fs';
 import * as readline from 'readline';
 import axios from 'axios';
@@ -17,33 +18,33 @@ readline
           /<h2> USACO \d+ (?:December|January|February|(?:US Open)) Contest, (\w+) <\/h2>/
         )[1];
         console.log(title, division);
+        const newEntry = {
+          uniqueId: `usaco-${id}`,
+          name: title,
+          url: `http://usaco.org/index.php?page=viewproblem2&cpid=${id}`,
+          source: division,
+          difficulty: 'Easy',
+          isStarred: false,
+          tags: [],
+          solutionMetadata: {
+            kind: 'USACO',
+            usacoId: id,
+          },
+        };
+        console.log(newEntry);
+        const extraProblems = JSON.parse(
+          readFileSync('./content/extraProblems.json')
+        );
+        extraProblems.EXTRA_PROBLEMS.push(newEntry);
+        writeFileSync(
+          './content/extraProblems.json',
+          JSON.stringify(extraProblems, null, '\t'),
+          'utf8'
+        );
         console.log('Problem added!');
         process.exit();
       })
       .catch(error => {
         console.error('Error retrieving HTML content:', error);
       });
-    // const newEntry = {
-    //   uniqueId: `usaco-${id}`,
-    //   name: 'Equal Sum Subarrays',
-    //   url: `http://usaco.org/index.php?page=viewproblem2&cpid=${id}`,
-    //   source: 'Gold',
-    //   difficulty: 'Easy',
-    //   isStarred: false,
-    //   tags: ['Brute Force'],
-    //   solutionMetadata: {
-    //     kind: 'USACO',
-    //     usacoId: id,
-    //   },
-    // };
-    // const extraProblems = JSON.parse(
-    //   readFileSync('./content/extraProblems.json')
-    // );
-    // console.log(extraProblems);
-    // extraProblems.EXTRA_PROBLEMS.push(newEntry);
-    // writeFileSync(
-    //   './content/extraProblems.json',
-    //   JSON.stringify(extraProblems, null, '\t'),
-    //   'utf8'
-    // );
   });
