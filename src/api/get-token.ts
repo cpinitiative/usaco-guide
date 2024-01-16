84df92ef5bbd897439a089166f265869a450881e
devo1ution <danielfangzhu@gmail.com>
2024-01-16 13:26:30 -0800

    remove @vercel/node

--- src/api/get-token.ts
+++ src/api/get-token.ts
@@ -1,20 +1,22 @@
 import { OAuthApp } from '@octokit/oauth-app';
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { GatsbyFunctionRequest, GatsbyFunctionResponse } from 'gatsby';
 
 const app = new OAuthApp({
   clientType: 'github-app',
   clientId: process.env.GATSBY_EDITOR_CLIENT_ID ?? '',
   clientSecret: process.env.EDITOR_CLIENT_SECRET ?? '',
 });
-
+interface RequestBody {
+  code: string;
+}
 export default async function handler(
-  request: VercelRequest,
-  response: VercelResponse
+  request: GatsbyFunctionRequest<RequestBody>,
+  response: GatsbyFunctionResponse
 ) {
   const {
     authentication: { token },
   } = await app.createToken({
-    code: request.body.code as string,
+    code: request.body.code,
   });
   response.json({ token });
 }
