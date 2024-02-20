3e6a51c9b95dcf6dd2096f1382d6dc120de13dda
devo1ution <danielfangzhu@gmail.com>
2024-02-20 08:53:56 -0800

    fix flicker?

--- src/components/Editor/BaseEditor.tsx
+++ src/components/Editor/BaseEditor.tsx
@@ -1,5 +1,6 @@
 import MonacoEditor from '@monaco-editor/react';
 import React from 'react';
+// can only be run on client, use only in lazy components
 export default function BaseEditor(props) {
   // prevents light/dark mode flicker
   const darkMode = document.documentElement.classList.contains('dark');
--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useIsUserDataLoaded } from './UserDataContext/UserDataContext';
 
 // Note: this is separate from DarkModeProvider.tsx because this is imported by Storybook.
 
@@ -6,6 +7,9 @@ export const DarkModeContext = React.createContext<boolean>(null);
 
 export const useDarkMode = (): boolean => {
   const context = React.useContext(DarkModeContext);
+  if (!useIsUserDataLoaded()) {
+    return document.documentElement.classList.contains('dark');
+  }
   if (context === null) {
     throw new Error('useDarkMode() must be used inside a DarkModeProvider');
   }
