d6624bec76d39f5b2aa36e76ee56e53d3dd2a615
devo1ution <danielfangzhu@gmail.com>
2024-02-20 22:19:55 -0800

    update editor as well

--- src/components/Editor/BaseEditor.tsx
+++ src/components/Editor/BaseEditor.tsx
@@ -1,8 +1,6 @@
 import MonacoEditor from '@monaco-editor/react';
 import React from 'react';
-// can only be run on client, use only in lazy components
+
 export default function BaseEditor(props) {
-  // prevents light/dark mode flicker
-  const darkMode = document.documentElement.classList.contains('dark');
-  return <MonacoEditor {...props} theme={darkMode ? 'vs-dark' : 'light'} />;
+  return <MonacoEditor {...props} />;
 }
