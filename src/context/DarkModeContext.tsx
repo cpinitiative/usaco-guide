7bd9c256d2eec548b565cc4db7b5226dc6869342
devo1ution <danielfangzhu@gmail.com>
2024-02-20 22:11:56 -0800

    fixes

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useIsUserDataLoaded } from './UserDataContext/UserDataContext';
 
 // Note: this is separate from DarkModeProvider.tsx because this is imported by Storybook.
 
@@ -7,9 +6,6 @@ export const DarkModeContext = React.createContext<boolean>(null);
 
 export const useDarkMode = (): boolean => {
   const context = React.useContext(DarkModeContext);
-  if (!useIsUserDataLoaded()) {
-    return document.documentElement.classList.contains('dark');
-  }
   if (context === null) {
     throw new Error('useDarkMode() must be used inside a DarkModeProvider');
   }
