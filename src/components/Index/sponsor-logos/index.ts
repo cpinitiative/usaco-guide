dc8bb2dbcb376ee770e95382e71936d61fabbf08
ryanchou-dev <ryanchou111@gmail.com>
2025-01-29 01:36:47 -0800

    non-trivial sponsor placement

--- src/components/Index/sponsor-logos/index.ts
+++ src/components/Index/sponsor-logos/index.ts
@@ -1,3 +1,4 @@
 export { default as EasyFunCoding } from './EasyFunCoding';
 export { default as Vercel } from './Vercel';
 export { default as XCamp } from './XCamp';
+export { default as NonTrivial } from './NonTrivial';
