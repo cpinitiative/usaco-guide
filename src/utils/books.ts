28b0ce6ded2b691af7671d42dba1ca3e7ad9f6e8
freakin23 <rameezparwez@gmail.com>
2025-02-13 19:35:08 +0530

    update books.ts

--- src/utils/books.ts
+++ src/utils/books.ts
@@ -7,7 +7,6 @@ export const books = {
   CPH: ['/CPH.pdf', "Competitive Programmer's Handbook (Antti Laaksonen)"],
   IUSACO: ['https://darrenyao.com/', 'An Introduction to USACO (Darren Yao)'],
   GCP: [
-    'https://link.springer.com/book/10.1007/978-3-319-72547-5',
-    'Guide to Competitive Programming (Antti Laaksonen)',
+    '/GCP.pdf', 'Guide to Competitive Programming (Antti Laaksonen)',
   ],
 };
