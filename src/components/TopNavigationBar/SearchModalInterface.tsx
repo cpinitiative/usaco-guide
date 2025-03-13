9b19d3341320f8a459a3ee8f4d8bc0ed4f5ea5ee
Nathan Wang <nathan.r.wang@gmail.com>
2025-03-12 23:49:05 -0400

    Migrate Search Modal Interface

--- src/components/TopNavigationBar/SearchModalInterface.tsx
+++ src/components/TopNavigationBar/SearchModalInterface.tsx
@@ -10,35 +10,9 @@ import {
   useHits,
   useSearchBox,
 } from 'react-instantsearch';
-import styled from 'styled-components';
-import tw from 'twin.macro';
 import { SECTION_LABELS, moduleIDToURLMap } from '../../../content/ordering';
 import { AlgoliaModuleInfoHit } from '../../models/module';
 import { searchClient } from '../../utils/algoliaSearchClient';
-
-const SearchResultDescription = styled.p`
-  ${tw`leading-4`}
-
-  > p > .ais-Highlight > * {
-    ${tw`text-gray-700`}
-    ${tw`text-sm!`}
-  }
-
-  .dark & > p > .ais-Highlight > * {
-    ${tw`text-gray-300`}
-  }
-
-  > .ais-Snippet > * {
-    ${tw`text-gray-400`}
-    ${tw`text-sm!`}
-  }
-`;
-
-const SearchResultsContainer = styled.div`
-  .dark & .ais-PoweredBy {
-    ${tw`text-dark-high-emphasis!`}
-  }
-`;
 
 const indexName = `${process.env.GATSBY_ALGOLIA_INDEX_NAME ?? 'dev'}_modules`;
 
@@ -63,31 +37,36 @@ const ModuleSearch = () => {
       </div>
       {query !== '' && (
         <div>
-          <SearchResultsContainer>
-            <div className="max-h-[20rem] sm:max-h-[40rem] overflow-y-auto border-t divide-y divide-gray-200 border-gray-200 dark:divide-gray-700 dark:border-gray-700">
-              {hits.map(hit => (
-                <Link
-                  to={moduleIDToURLMap[hit.id]}
-                  className="block hover:bg-blue-100 dark:hover:bg-gray-700 py-3 px-5 transition"
-                  key={hit.id}
-                >
-                  <h3 className="text-gray-600 dark:text-gray-200 font-medium">
-                    <Highlight hit={hit} attribute="title" /> -{' '}
-                    {SECTION_LABELS[hit.division]}
-                  </h3>
-                  <SearchResultDescription>
-                    <p className="mb-1">
-                      <Highlight hit={hit} attribute="description" />
-                    </p>
+          <div className="max-h-[20rem] sm:max-h-[40rem] overflow-y-auto border-t divide-y divide-gray-200 border-gray-200 dark:divide-gray-700 dark:border-gray-700">
+            {hits.map(hit => (
+              <Link
+                to={moduleIDToURLMap[hit.id]}
+                className="block hover:bg-blue-100 dark:hover:bg-gray-700 py-3 px-5 transition"
+                key={hit.id}
+              >
+                <h3 className="text-gray-700 dark:text-gray-200 font-medium">
+                  <Highlight hit={hit} attribute="title" /> -{' '}
+                  {SECTION_LABELS[hit.division]}
+                </h3>
+                <p className="[&_*]:!text-sm [&_*]:!leading-4">
+                  <div className="mb-1 text-gray-700 dark:text-gray-200">
+                    <Highlight hit={hit} attribute="description" />
+                  </div>
+                  <div className="text-gray-500 dark:text-gray-400">
                     <Snippet hit={hit} attribute="content" />
-                  </SearchResultDescription>
-                </Link>
-              ))}
-            </div>
-            <div className="px-5 py-3 border-t border-gray-200 dark:border-gray-700">
+                  </div>
+                </p>
+              </Link>
+            ))}
+          </div>
+          <div className="px-5 py-3 border-t border-gray-200 dark:border-gray-700">
+            <div className="hidden dark:block">
               <PoweredBy theme="dark" />
             </div>
-          </SearchResultsContainer>
+            <div className="block dark:hidden">
+              <PoweredBy theme="light" />
+            </div>
+          </div>
         </div>
       )}
     </div>
