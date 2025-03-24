365778c363d10cf192275f6d7b91463b7afa2202
Nathan Wang <nathan.r.wang@gmail.com>
2025-03-23 17:34:05 -0700

    prep

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    'tailwindcss/nesting',
     require(`tailwindcss`),
-    require(`autoprefixer`),
     ...(process.env.NODE_ENV === `production` ? [require(`cssnano`)] : []),
   ],
 };
