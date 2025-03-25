522e9427c3bc18c72e25d73782214d0a19de40f2
Nathan Wang <nathan.r.wang@gmail.com>
2025-03-24 22:30:58 -0400

    migrate resources dropdown

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -1,4 +1,9 @@
-import { Popover, Transition } from '@headlessui/react';
+import {
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+  Transition,
+} from '@headlessui/react';
 import {
   AcademicCapIcon,
   BookmarkIcon,
@@ -177,97 +182,80 @@ export default function TopNavigationBar({
                 >
                   Problems
                 </Link>
-                <Popover.Group as="nav" className="h-full">
-                  <Popover className="h-full">
-                    {({ open }) => (
-                      <>
-                        <Popover.Button
-                          className={classNames(
-                            open
-                              ? 'text-gray-900'
-                              : 'text-gray-500 hover:border-gray-300 focus:border-gray-300 dark:hover:border-gray-500 dark:focus:border-gray-500',
-                            'group inline-flex items-center h-full border-b-2 border-transparent space-x-2 text-base leading-6 font-medium hover:text-gray-900 focus:outline-hidden focus:text-gray-900  transition ease-in-out duration-150 dark:text-dark-high-emphasis'
-                          )}
-                        >
-                          <span className="mt-0.5">Resources</span>
-                          <ChevronDownIcon
-                            className={classNames(
-                              open ? 'text-gray-500' : 'text-gray-400',
-                              'mt-0.5 h-5 w-5 group-hover:text-gray-500 group-focus:text-gray-500 dark:text-dark-med-emphasis dark:group-hover:text-dark-med-emphasis dark:group-focus:text-dark-med-emphasis transition ease-in-out duration-150'
-                            )}
-                            aria-hidden="true"
-                          />
-                        </Popover.Button>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-200"
-                          enterFrom="opacity-0 translate-y-1"
-                          enterTo="opacity-100 translate-y-0"
-                          leave="transition ease-in duration-150"
-                          leaveFrom="opacity-100 translate-y-0"
-                          leaveTo="opacity-0 translate-y-1"
-                        >
-                          <Popover.Panel
-                            static
-                            className="hidden md:block z-20 shadow-lg absolute left-1/2 transform -translate-x-1/2 -mt-2 px-2 w-screen max-w-md sm:px-0 lg:max-w-3xl"
-                          >
-                            <div className="rounded-lg shadow-lg ring-1 ring-black/5 overflow-hidden">
-                              <div className="relative grid gap-6 bg-white dark:bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
-                                <Link
-                                  to="/groups/"
-                                  className="-m-3 p-3 flex items-start rounded-lg dark:hover:bg-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-                                >
-                                  <div className="shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-blue-500 text-white sm:h-12 sm:w-12">
-                                    <UserGroupIcon
-                                      className="h-6 w-6"
-                                      aria-hidden="true"
-                                    />
-                                  </div>
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900 dark:text-dark-high-emphasis">
-                                      Groups
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500 dark:text-dark-med-emphasis">
-                                      A Learning Management System fully
-                                      integrated with the USACO Guide.
-                                    </p>
-                                  </div>
-                                </Link>
-                                {resources.map(item => (
-                                  <a
-                                    key={item.name}
-                                    href={item.href}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="-m-3 p-3 flex items-start rounded-lg dark:hover:bg-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-                                  >
-                                    <div className="shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-blue-500 text-white sm:h-12 sm:w-12">
-                                      <item.icon
-                                        className="h-6 w-6"
-                                        aria-hidden="true"
-                                      />
-                                    </div>
-                                    <div className="ml-4">
-                                      <div className="flex text-base font-medium text-gray-900 dark:text-dark-high-emphasis">
-                                        {item.name}{' '}
-                                        <span className="text-gray-400 mt-0.5 ml-2 h-5 w-5">
-                                          <ExternalLinkIcon />
-                                        </span>
-                                      </div>
-                                      <p className="mt-1 text-sm text-gray-500 dark:text-dark-med-emphasis">
-                                        {item.description}
-                                      </p>
-                                    </div>
-                                  </a>
-                                ))}
-                              </div>
-                            </div>
-                          </Popover.Panel>
-                        </Transition>
-                      </>
+
+                <Popover className="h-full">
+                  <PopoverButton
+                    className={classNames(
+                      'text-gray-500 hover:border-gray-300 focus:border-gray-300 dark:hover:border-gray-500 dark:focus:border-gray-500',
+                      'group inline-flex items-center h-full border-b-2 border-transparent space-x-2 text-base leading-6 font-medium hover:text-gray-900 focus:outline-hidden focus:text-gray-900  transition ease-in-out duration-150 dark:text-dark-high-emphasis'
                     )}
-                  </Popover>
-                </Popover.Group>
+                  >
+                    <span className="mt-0.5">Resources</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        'text-gray-400',
+                        'mt-0.5 h-5 w-5 group-hover:text-gray-500 group-focus:text-gray-500 dark:text-dark-med-emphasis dark:group-hover:text-dark-med-emphasis dark:group-focus:text-dark-med-emphasis transition ease-in-out duration-150'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </PopoverButton>
+                  <PopoverPanel
+                    transition
+                    className="hidden md:block z-20 shadow-lg absolute left-1/2 transform -translate-x-1/2 -mt-2 px-2 w-screen max-w-md sm:px-0 lg:max-w-3xl transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+                  >
+                    <div className="rounded-lg shadow-lg ring-1 ring-black/5 overflow-hidden">
+                      <div className="relative grid gap-6 bg-white dark:bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
+                        <Link
+                          to="/groups/"
+                          className="-m-3 p-3 flex items-start rounded-lg dark:hover:bg-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
+                        >
+                          <div className="shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-blue-500 text-white sm:h-12 sm:w-12">
+                            <UserGroupIcon
+                              className="h-6 w-6"
+                              aria-hidden="true"
+                            />
+                          </div>
+                          <div className="ml-4">
+                            <p className="text-base font-medium text-gray-900 dark:text-dark-high-emphasis">
+                              Groups
+                            </p>
+                            <p className="mt-1 text-sm text-gray-500 dark:text-dark-med-emphasis">
+                              A Learning Management System fully integrated with
+                              the USACO Guide.
+                            </p>
+                          </div>
+                        </Link>
+                        {resources.map(item => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="-m-3 p-3 flex items-start rounded-lg dark:hover:bg-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
+                          >
+                            <div className="shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-blue-500 text-white sm:h-12 sm:w-12">
+                              <item.icon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="ml-4">
+                              <div className="flex text-base font-medium text-gray-900 dark:text-dark-high-emphasis">
+                                {item.name}{' '}
+                                <span className="text-gray-400 mt-0.5 ml-2 h-5 w-5">
+                                  <ExternalLinkIcon />
+                                </span>
+                              </div>
+                              <p className="mt-1 text-sm text-gray-500 dark:text-dark-med-emphasis">
+                                {item.description}
+                              </p>
+                            </div>
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </PopoverPanel>
+                </Popover>
                 <button
                   className="cursor-pointer inline-flex items-center px-1 border-b-2 border-transparent text-base font-medium leading-6 text-gray-500 hover:text-gray-900 hover:border-gray-300 focus:outline-hidden focus:text-gray-900 focus:border-gray-300 dark:text-dark-high-emphasis dark:hover:border-gray-500 dark:focus:border-gray-500 transition"
                   onClick={() => setIsContactUsActive(true)}
