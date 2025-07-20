4d1526f57737ebc9f7da4ad11f5af798193f03ef
emhgit <eharper0815@gmail.com>
2025-07-19 22:40:50 -0500

    wrap dialogPanel in a wrapper div in the ProblemSubmissonPopup modal

--- src/components/Groups/ProblemSubmissionPopup.tsx
+++ src/components/Groups/ProblemSubmissionPopup.tsx
@@ -50,105 +50,107 @@ function ProblemSubmissionPopup() {
         className="fixed inset-0 bg-gray-500/75 dark:bg-gray-800/75"
         aria-hidden="true"
       />
-      <div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-        <span
-          className="hidden sm:inline-block sm:h-screen sm:align-middle"
-          aria-hidden="true"
-        >
-          &#8203;
-        </span>
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+          <span
+            className="hidden sm:inline-block sm:h-screen sm:align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
 
-        <DialogPanel
-          transition
-          className="dark:bg-dark-surface inline-block transform overflow-hidden rounded-lg bg-white pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl sm:py-6 sm:align-middle"
-          role="dialog"
-          aria-modal="true"
-          aria-labelledby="modal-headline"
-        >
-          <div className="px-4 sm:px-6">
-            <div className="flex items-start justify-between">
-              <h3
-                className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100"
-                id="modal-headline"
-              >
-                Submission Details
-              </h3>
-              <div className="ml-3 flex h-7 items-center">
-                <button
-                  type="button"
-                  className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500 focus:outline-none dark:bg-gray-800 dark:text-gray-400"
-                  onClick={() => popupContext.setShowPopup(false)}
+          <DialogPanel
+            transition
+            className="dark:bg-dark-surface inline-block transform overflow-hidden rounded-lg bg-white pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl sm:py-6 sm:align-middle"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-headline"
+          >
+            <div className="px-4 sm:px-6">
+              <div className="flex items-start justify-between">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100"
+                  id="modal-headline"
                 >
-                  <span className="sr-only">Close</span>
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+                  Submission Details
+                </h3>
+                <div className="ml-3 flex h-7 items-center">
+                  <button
+                    type="button"
+                    className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500 focus:outline-none dark:bg-gray-800 dark:text-gray-400"
+                    onClick={() => popupContext.setShowPopup(false)}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
+                    <span className="sr-only">Close</span>
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
 
-            <div className="mt-4">
-              <div className="overflow-hidden shadow sm:rounded-lg">
-                <div className="bg-white px-4 py-5 sm:p-6 dark:bg-gray-800">
-                  <div className="flex items-center justify-between">
-                    <div>
-                      <h4 className="text-lg font-medium text-gray-900 dark:text-gray-100">
-                        {getSubmissionStatus(submission)}
-                      </h4>
-                      <p className="text-sm text-gray-500 dark:text-gray-400">
-                        {getSubmissionTimestampString(submission)}
-                      </p>
+              <div className="mt-4">
+                <div className="overflow-hidden shadow sm:rounded-lg">
+                  <div className="bg-white px-4 py-5 sm:p-6 dark:bg-gray-800">
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <h4 className="text-lg font-medium text-gray-900 dark:text-gray-100">
+                          {getSubmissionStatus(submission)}
+                        </h4>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">
+                          {getSubmissionTimestampString(submission)}
+                        </p>
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="mt-4">
-                    <h5 className="text-sm font-medium text-gray-900 dark:text-gray-100">
-                      Code
-                    </h5>
-                    <div className="mt-2">
-                      {isCodeSubmission(submission) ? (
-                        <CodeBlock
-                          className={`language-${submission.language}`}
-                          isDarkMode={isDarkMode}
-                        >
-                          {submissionResult?.sourceCode ?? submission.code}
-                        </CodeBlock>
-                      ) : (
-                        <CodeBlock
-                          className="language-plaintext"
-                          isDarkMode={isDarkMode}
-                        >
-                          {submissionResult?.sourceCode ?? 'Loading...'}
-                        </CodeBlock>
-                      )}
+                    <div className="mt-4">
+                      <h5 className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                        Code
+                      </h5>
+                      <div className="mt-2">
+                        {isCodeSubmission(submission) ? (
+                          <CodeBlock
+                            className={`language-${submission.language}`}
+                            isDarkMode={isDarkMode}
+                          >
+                            {submissionResult?.sourceCode ?? submission.code}
+                          </CodeBlock>
+                        ) : (
+                          <CodeBlock
+                            className="language-plaintext"
+                            isDarkMode={isDarkMode}
+                          >
+                            {submissionResult?.sourceCode ?? 'Loading...'}
+                          </CodeBlock>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="mt-5 bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 dark:bg-gray-900">
-            <button
-              type="button"
-              className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200"
-              onClick={() => popupContext.setShowPopup(false)}
-            >
-              Close Submission
-            </button>
-          </div>
-        </DialogPanel>
+            <div className="mt-5 bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 dark:bg-gray-900">
+              <button
+                type="button"
+                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200"
+                onClick={() => popupContext.setShowPopup(false)}
+              >
+                Close Submission
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
       </div>
     </Dialog>
   );
