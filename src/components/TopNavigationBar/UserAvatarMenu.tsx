280107a0b007aa193f4c1eb6534689f6373a2382
emhgit <eharper0815@gmail.com>
2025-07-21 12:51:55 -0500

    use v2 syntax in UserAvatarMenu.tsx

--- src/components/TopNavigationBar/UserAvatarMenu.tsx
+++ src/components/TopNavigationBar/UserAvatarMenu.tsx
@@ -1,14 +1,7 @@
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
-} from '@headlessui/react';
-import classNames from 'classnames';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import type { User } from 'firebase/auth';
 import { Link } from 'gatsby';
-import React, { Fragment } from 'react';
+import React from 'react';
 
 export interface UserAvatarMenuProps {
   firebaseUser: User;
@@ -31,53 +24,36 @@ export const UserAvatarMenu: React.FC<UserAvatarMenuProps> = props => {
             </MenuButton>
           </div>
 
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
+          {/* Remove <Transition> and use transition prop on MenuItems */}
+          <MenuItems
+            transition
+            className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition ease-out focus:outline-hidden data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 dark:bg-gray-800"
           >
-            <MenuItems
-              static
-              className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-hidden dark:bg-gray-800"
-            >
-              <div className="py-1">
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to="/settings"
-                      className={classNames(
-                        active && 'bg-gray-100 dark:bg-gray-700',
-                        'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 focus:outline-hidden dark:text-gray-100'
-                      )}
-                    >
-                      Settings
-                    </Link>
-                  )}
-                </MenuItem>
-                <MenuItem>
-                  {({ active }) => (
-                    <button
-                      onClick={() => {
-                        props.onSignOut();
-                      }}
-                      className={classNames(
-                        active && 'bg-gray-100 dark:bg-gray-700',
-                        'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 focus:outline-hidden dark:text-gray-100'
-                      )}
-                      role="menuitem"
-                    >
-                      Sign out
-                    </button>
-                  )}
-                </MenuItem>
-              </div>
-            </MenuItems>
-          </Transition>
+            <div className="py-1">
+              <MenuItem>
+                {({ focus }) => (
+                  <span
+                    className={`block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 focus:outline-hidden dark:text-gray-100${focus ? 'bg-gray-100 dark:bg-gray-700' : ''}`}
+                  >
+                    <Link to="/settings">Settings</Link>
+                  </span>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ focus }) => (
+                  <button
+                    onClick={() => {
+                      props.onSignOut();
+                    }}
+                    role="menuitem"
+                    className={`block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 focus:outline-hidden dark:text-gray-100${focus ? 'bg-gray-100 dark:bg-gray-700' : ''}`}
+                  >
+                    Sign out
+                  </button>
+                )}
+              </MenuItem>
+            </div>
+          </MenuItems>
         </>
       )}
     </Menu>
