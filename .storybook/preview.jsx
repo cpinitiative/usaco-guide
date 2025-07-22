79c9baa66210014938ef048092c16d52cf431dd8
emhgit <eharper0815@gmail.com>
2025-07-21 20:14:06 -0500

    change usaco-1371 solutionMetadata.kind to internal and delete old preview.js

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -3,7 +3,7 @@ import { DarkModeContext } from '../src/context/DarkModeContext';
 import '../src/styles/main.css';
 import './storybook.css';
 
-import { action } from 'storybook/actions';
+import { action } from 'storybook/preview-api';
 
 // Gatsby's Link overrides:
 // Gatsby Link calls the `enqueue` & `hovering` methods on the global variable ___loader.
