900882fcb93c1a70cdf69fe6e41dc8db25df0fb8
freakin23 <rameezparwez@gmail.com>
2025-10-14 22:04:57 +0530

    not necessary

--- src/api/(parsers)/cc.ts
+++ src/api/(parsers)/cc.ts
@@ -1,12 +1,5 @@
 // example url: https://www.codechef.com/problems/GLADFIGHT
 export default function parseCC(url: string, html: string) {
-
-  const ccUrlPattern = /^(https:\/\/)?(www\.)?codechef\.com\/problems\/[A-Za-z0-9_]+$/;
-
-  if (!ccUrlPattern.test(url)) {
-    throw new Error(`Invalid CodeChef link. Expected format: https://www.codechef.com/problems/PROBLEM_ID`);
-  }
-
   const urlSplit = url.split('/');
   const uniqueId = urlSplit.at(-1);
   const titleRegex = /<title>(.*?)<\/title>/;
