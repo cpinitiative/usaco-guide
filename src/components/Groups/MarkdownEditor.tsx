946b284fa85a9eb9a2ab291faf7ed16f0103c3bb
emhgit <eharper0815@gmail.com>
2026-03-18 22:19:11 -0500

    memoize options parameter to prevent cursor focusing and positioning bugs in md editor

--- src/components/Groups/MarkdownEditor.tsx
+++ src/components/Groups/MarkdownEditor.tsx
@@ -3,48 +3,20 @@ import { useDarkMode } from '../../context/DarkModeContext';
 
 const SimpleMDE = React.lazy(() => import('react-simplemde-editor'));
 
-export default function MarkdownEditor({ value, onChange }) {
+export default function MarkdownEditor({
+  value,
+  onChange,
+  options,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+  options?: any;
+}) {
   const darkMode = useDarkMode();
-
   return (
     <div className={darkMode ? 'dark-mde-container' : 'mde-container'}>
       <React.Suspense fallback={<div>Loading editor...</div>}>
-        <SimpleMDE
-          onChange={onChange}
-          value={value}
-          options={{
-            toolbar: [
-              'bold',
-              'italic',
-              'strikethrough',
-              '|',
-              'heading-1',
-              'heading-2',
-              'heading-3',
-              '|',
-              'link',
-              'image',
-              '|',
-              'quote',
-              {
-                name: 'custom',
-                action: function customFunction(editor) {
-                  editor.codemirror.replaceSelection('```java\n\n```');
-                },
-                className: 'fa fa-code',
-                title: 'Custom Button',
-              },
-              '|',
-              'ordered-list',
-              'unordered-list',
-              'table',
-            ],
-            shortcuts: {
-              toggleSideBySide: null,
-              toggleFullScreen: null,
-            },
-          }}
-        />
+        <SimpleMDE onChange={onChange} value={value} options={options} />
       </React.Suspense>
     </div>
   );
