d9658dc5bc89770aa2fc78aaf26964c732ee34e4
Sosuke23 <rameezparwez@gmail.com>
2026-08-02 08:43:22 +0000

    navigation shortcut

--- src/components/MarkdownLayout/NavBar.tsx
+++ src/components/MarkdownLayout/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import * as React from 'react';
+import { useRouter } from 'next/router';
 import MODULE_ORDERING from '../../../content/ordering';
 import { useMarkdownLayout } from '../../context/MarkdownLayoutContext';
 import { MarkdownLayoutSidebarModuleLinkInfo } from '../../models/module';
@@ -25,14 +26,51 @@ const NavBar = ({ alignNavButtonsRight = true }) => {
     () => sortedModuleLinks?.findIndex(x => x.id === markdownLayoutInfo.id),
     [markdownLayoutInfo, sortedModuleLinks]
   ) as number;
-  if (!sortedModuleLinks || markdownLayoutInfo instanceof SolutionInfo) {
-    return null;
-  }
+
   const prevModule = moduleIdx === 0 ? null : sortedModuleLinks[moduleIdx - 1];
   const nextModule =
     moduleIdx === sortedModuleLinks.length - 1
       ? null
       : sortedModuleLinks[moduleIdx + 1];
+
+   React.useEffect(() => {
+
+    if (!sortedModuleLinks || markdownLayoutInfo instanceof SolutionInfo) {
+      return;
+    } 
+
+    const navigationShortcut = (e: KeyboardEvent) => {
+      
+      if (!e.ctrlKey) {
+        return;
+      } 
+
+      const target = e.target as HTMLElement;
+      const htmlTag = target?.tagName;
+      if (htmlTag === 'INPUT' || htmlTag === 'TEXTAREA' || target?.isContentEditable) {
+        return;
+      }
+
+      if (e.key === 'ArrowRight') {
+        e.preventDefault();
+        if (nextModule) {
+          router.push(nextModule.url);
+        }    
+      } else if (e.key === 'ArrowLeft') {
+        e.preventDefault();
+        if (prevModule) {
+          router.push(prevModule.url);
+        } 
+      }
+    };
+
+    window.addEventListener('keydown', navigationShortcut);
+    return () => window.removeEventListener('keydown', navigationShortcut);
+  }, [prevModule, nextModule, router, sortedModuleLinks, markdownLayoutInfo]);
+
+  if (!sortedModuleLinks || markdownLayoutInfo instanceof SolutionInfo) {
+    return null;
+  }
 
   const disabledClasses =
     'text-gray-200 pointer-events-none dark:text-dark-disabled-emphasis';
@@ -52,6 +90,7 @@ const NavBar = ({ alignNavButtonsRight = true }) => {
             'inline-flex items-center rounded-md px-4 py-2 text-sm leading-5 font-medium ' +
             (prevModule === null ? disabledClasses : activeClasses)
           }
+          title = "Prev (Ctrl + <-)"
         >
           <svg
             className="mr-1 -ml-0.5 h-4 w-4"
@@ -77,6 +116,7 @@ const NavBar = ({ alignNavButtonsRight = true }) => {
             'inline-flex items-center rounded-md px-4 py-2 text-sm leading-5 font-medium ' +
             (nextModule === null ? disabledClasses : activeClasses)
           }
+          title = "Next (Ctrl + ->)"
         >
           Next
           <svg
